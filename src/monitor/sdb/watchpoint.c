356eb16e453edff12091b8ad1fc83f731ce4f7a0
tracer-ics2024 <tracer@njuics.org>
2025-11-08 11:39:32 +0800

    >  compile NEMU 100017119 林俊杰 Linux DESKTOP-PAP9J8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  11:39:32 up  3:20,  1 user,  load average: 0.00, 0.01, 0.00

--- src/monitor/sdb/watchpoint.c
+++ src/monitor/sdb/watchpoint.c
@@ -1,37 +1,42 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include "sdb.h"
 
 #define NR_WP 32
 
-typedef struct watchpoint {
+typedef struct watchpoint
+{
   int NO;
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-  char expr[32];//The expression for monitoring
-  uint32_t old_val;//The value of the last time
+  char expr[32];    // The expression for monitoring
+  uint32_t old_val; // The value of the last time
 } WP;
 
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
 
-void init_wp_pool() {
+WP *new_wp();
+void free_wp(WP *wp);
+void init_wp_pool()
+{
   int i;
-  for (i = 0; i < NR_WP; i ++) {
+  for (i = 0; i < NR_WP; i++)
+  {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
   }
@@ -41,25 +46,78 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
-/**
- * @brief Print all currently set watchpoint information
- *
- * Traverse the linked list of active watchpoints and print each watchpoint's
- * index, expression, and current value.
- * If no watchpoints are set, print a message indicating this.
- */
-void PrintWatchPoint(){
-  if (head == NULL) {
+
+void PrintWatchPoint()
+{
+  if (head == NULL)
+  {
     printf("monitor watchpoint file PrintWatchPoint function check, detect head==NULL\n");
     printf("No watchpoints are set.\n");
-    return;//this return can direct return this function.
+    return; // this return can direct return this function.
   }
   printf("Num\tExpr\t\tValue\n");
   printf("----\t----\t\t-----\n");
   WP *p = head;
-  while (p!=NULL)
+  while (p != NULL)
   {
     printf("%d\t%s\t\t0x%08x\n", p->NO, p->expr, p->old_val);
     p = p->next;
   }
+}
+WP *new_wp()
+{
+  // check NULL
+  assert(free_ != NULL);
+  WP *wp = free_;
+  free_ = free_->next;
+  wp->next = head;
+  head = wp;
+  return wp;
+}
+void free_wp(WP *wp)
+{
+  // check null args
+  if (wp == NULL)
+  {
+    printf("Warning: Attempt to free a NULL watchpoint\n");
+    return;
+  }
+  if (head == NULL)
+  {
+    printf("Warning: No watchpoints in use, cannot free WP #%d\n", wp->NO);
+    return;
+  }
+  WP *previous = NULL;
+  WP *current = head;
+  // search goal node and before node
+  while (current != NULL && current != wp)
+  {
+    previous = current;
+    current = current->next;
+  }
+  // if not found this node
+  if (current == NULL)
+  {
+    printf("Warning: Watchpoint #%d is not in the active list\n", wp->NO);
+    return;
+  }
+  if (previous == NULL)
+  {
+    head = wp->next; // head point next node
+  }
+  else if (previous->next == wp)
+  {
+    previous->next = wp->next;
+  }
+  else
+  {
+    printf("I do not know watchpoint.c list happend something\n");
+    printf("Maybe WP#%d not reachable from prev node\n", wp->NO);
+    assert(0); // direct exit
+  }
+  // clear data
+  wp->expr[0] = '\0';
+  wp->old_val = 0;
+  wp->next = free_;
+  free_ = wp;
 }
