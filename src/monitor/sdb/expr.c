e5c022cd0fa3830490d30c067a63cb0ce09208c3
tracer-ics2024 <tracer@njuics.org>
2025-11-10 19:12:34 +0800

    >  compile NEMU 100017119 林俊杰 Linux DESKTOP-PAP9J8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  19:12:34 up 17 min,  1 user,  load average: 0.16, 0.09, 0.08

--- src/monitor/sdb/expr.c
+++ src/monitor/sdb/expr.c
@@ -145,6 +145,8 @@ static bool make_token(char *e)
         case TK_REG:
           strncpy(tokens[nr_token].str, substr_start, substr_len);
           tokens[nr_token].str[substr_len] = '\0';
+          tokens[nr_token].type = rules[i].token_type;
+          nr_token++;
           break;
         case TK_NOTYPE:
           break;
