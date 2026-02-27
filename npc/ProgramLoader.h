f5ea05b56058a1c7222e754d34a99490df543194
100017119-Lin Junjie <alsttabblt@outlook.com>
2026-02-27 10:07:01 +0800

    feat: initialize minirvEMUTestDebug

---
+++ npc/ProgramLoader.h
@@ -0,0 +1,18 @@
+#ifndef PROGRAM_LOADER_H
+#define PROGRAM_LOADER_H
+#include <vector>
+#include <cstdint>
+#include <filesystem>
+#include <string>
+#include <optional>
+class ProgramLoader
+{
+private:
+public:
+    ProgramLoader() = default;
+    ~ProgramLoader() = default;
+    // 返回包含字节数据
+    static std::optional<std::vector<uint8_t>> LoadBinary(const std::filesystem::path &filePath);
+    static std::optional<size_t> GetFileSize(const std::filesystem::path& filePath);
+};
+#endif
