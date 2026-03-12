96a95bf21b3aeaecbae068d96452bf81034bcfd5
tracer-ysyx <tracer@ysyx.org>
2026-03-12 15:05:13 +0800

    > compile NEMU ysyx_100017119 林俊杰 Linux DESKTOP-PAP9J8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  15:05:13 up  3:54,  1 user,  load average: 0.20, 0.14, 0.10

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -421,6 +421,10 @@ static int cmd_w(char *args)
     printf("  w *0x80100000    - Watch dereferenced memory\n");
     return 0;
   }
+  if(strlen(args)>32){
+    printf("The expression is too long.");
+    return 0;
+  }
   if (!validate_expression_syntax(args))
   {
     printf("Error: Expression syntax error\n");
