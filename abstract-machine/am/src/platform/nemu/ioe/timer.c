436d0f98ff5f5a157fd74c5cd04e026b6e79b78d
100017119-Lin Junjie <alsttabblt@outlook.com>
2026-04-07 16:35:30 +0800

    终于做完了真正的stdio和string和stdlib，这内存设计太难设计了，仿照Linux的页分配器的伙伴系统设计的

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,18 +1,33 @@
 #include <am.h>
 #include <nemu.h>
-
-void __am_timer_init() {
+// 自己写的
+static uint64_t boot_time = 0;
+static uint64_t read_time()
+{
+  uint32_t HighPart = inl(RTC_ADDR + 4);
+  uint32_t LowPart = inl(RTC_ADDR);
+  uint64_t Result = ((uint64_t)HighPart << 32) | LowPart;
+  return Result;
+}
+void __am_timer_init()
+{
+  // 自己写的
+  boot_time = read_time();
 }
 
-void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
+{
+  // uptime->us = 0;
+  // 自己写的
+  uptime->us = read_time() - boot_time;
 }
 
-void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
+void __am_timer_rtc(AM_TIMER_RTC_T *rtc)
+{
   rtc->second = 0;
   rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+  rtc->hour = 0;
+  rtc->day = 0;
+  rtc->month = 0;
+  rtc->year = 1900;
 }
