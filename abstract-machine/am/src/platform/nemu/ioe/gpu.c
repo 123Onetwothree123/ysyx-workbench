5db7c2c43bb25fb497a5b13565a9bfe14a12ed5d
tracer-ysyx <tracer@ysyx.org>
2026-04-19 12:42:49 +0800

    > compile NEMU ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  12:42:49 up  2:18,  1 user,  load average: 0.04, 0.15, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,48 +1,76 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-static int screen_w = 0;
-static int screen_h = 0;
-
-void __am_gpu_init() {
+void __am_gpu_init()
+{
+  /*
++  int i;
++  int w = 0;  // TODO: get the correct width
++  int h = 0;  // TODO: get the correct height
++  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
++  for (i = 0; i < w * h; i ++) fb[i] = i;
++  outl(SYNC_ADDR, 1);
+  */
+  uint32_t Information = inl(VGACTL_ADDR);
   int i;
-  AM_GPU_CONFIG_T cfg = io_read(AM_GPU_CONFIG);
-  screen_w = cfg.width;
-  screen_h = cfg.height;
+  int w = Information >> 16;    // TODO: get the correct width
+  int h = Information & 0xffff; // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < screen_w * screen_h; i ++) fb[i] = i;
+  for (i = 0; i < w * h; i++)
+    fb[i] = i;
   outl(SYNC_ADDR, 1);
 }
 
-void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t info = inl(VGACTL_ADDR);
-  screen_w = info >> 16;
-  screen_h = info & 0xffff;
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
+{
+  /*
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = screen_w, .height = screen_h,
-    .vmemsz = screen_w * screen_h * sizeof(uint32_t)
+    .width = 0, .height = 0,
+    .vmemsz = 0
   };
+  */
+  uint32_t Information = inl(VGACTL_ADDR);
+  uint32_t Width = Information >> 16;
+  uint32_t Height = Information & 0xffff;
+  *cfg = (AM_GPU_CONFIG_T){
+      .present = true, .has_accel = false, .width = Width, .height = Height, .vmemsz = Width * Height * sizeof(uint32_t)};
 }
 
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->pixels != NULL) {
-    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-    uint32_t *pixels = (uint32_t *)ctl->pixels;
-    int x = ctl->x, y = ctl->y, dw = ctl->w, dh = ctl->h;
-    for (int row = 0; row < dh; row++) {
-      for (int col = 0; col < dw; col++) {
-        fb[(y + row) * screen_w + (x + col)] = pixels[row * dw + col];
-      }
-    }
-  }
-  if (ctl->sync) {
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
+{
+  /*
+  if (ctl->sync)
+  {
     outl(SYNC_ADDR, 1);
   }
+  */
+  int32_t x = ctl->x;
+  int32_t y = ctl->y;
+  int32_t w = ctl->w;
+  int32_t h = ctl->h;
+  uint32_t *Pixels = ctl->pixels;
+  uint32_t ScreenWidth = inl(VGACTL_ADDR) >> 16;
+  uint32_t *FrameBuffer = (uint32_t *)(uintptr_t)FB_ADDR;
+  int32_t RowOffset = 0;    // 行
+  int32_t ColumnOffset = 0; // 列
+  for (RowOffset = 0; RowOffset < h; RowOffset++)
+  {
+    for (ColumnOffset = 0; ColumnOffset < w; ColumnOffset++)
+    {
+      /*
+      牛逼，他妈的研究了半天都没搞懂pixels一个一维数组如何表示，想起来了以前看过的C++ primer plus想起来了可以一行过去，定义从
+      哪里到哪里是一行，再从哪里到哪里是下一行，然后我又去问了下ai确认下能否这么设计，看起来是可以的，牛逼卧槽
+      */
+      FrameBuffer[(y + RowOffset) * ScreenWidth + (x + ColumnOffset)] = Pixels[RowOffset * w + ColumnOffset];
+    }
+  }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
+void __am_gpu_status(AM_GPU_STATUS_T *status)
+{
   status->ready = true;
 }
