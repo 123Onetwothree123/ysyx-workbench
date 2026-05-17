d1b6cfa6cbbc4f95b05e5dbf8bf0521b05df7784
100017119-Lin Junjie <alsttabblt@outlook.com>
2026-05-17 15:20:05 +0800

    update

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -32,8 +32,7 @@ static void restart() {
 
   /* The zero register is always 0. */
   cpu.gpr[0] = 0;
-
-  //ysyx要求的32 0x1800，64是0xa00001800
+  //项目组要求的32 0x1800，64是0xa00001800
   cpu.mstatus = MUXDEF(CONFIG_RV64, 0xa00001800UL, 0x1800UL);
 }
 
