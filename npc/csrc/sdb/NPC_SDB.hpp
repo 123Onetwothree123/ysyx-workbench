422b2fc65cfc75a5c0c426239ddc3aa32f0b0980
tracer-ysyx <tracer@ysyx.org>
2026-05-24 13:55:08 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  13:55:08 up  2:26,  1 user,  load average: 0.95, 0.38, 0.22

--- npc/csrc/sdb/NPC_SDB.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef NPC_SDB_HPP
-#define NPC_SDB_HPP
-#include <cstddef>
-class VRV32E32Reg;
-void sdb_main_loop(VRV32E32Reg &top, std::size_t &cycles, bool batch_mode = false);
-#endif
--- npc/csrc/trace/trace.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef TRACE_HPP
-#define TRACE_HPP
-#include "ftrace.hpp"
-#include "itrace.hpp"
-#include "mtrace.hpp"
-#endif
