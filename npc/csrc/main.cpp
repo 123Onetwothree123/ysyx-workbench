a6d1fd1d38a86dcd41acc6cc3605d725b1ab2490
tracer-ysyx <tracer@ysyx.org>
2026-05-30 17:16:36 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  17:16:36 up 59 min,  1 user,  load average: 0.03, 0.05, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -6,6 +6,18 @@
 #include "CliOptions.hpp"
 int main(int argc, char const *argv[])
 {
-
+    auto options{CliOptions::Parse(argc, argv)};
+    if (!options)
+    {
+        std::println(std::cerr, "{0}", options.error());
+        return 1;
+    }
+    Verilated::commandArgs(argc, argv);
+    const auto image_size{ImageLoader::LoadFromCli(*options)};
+    if (!image_size)
+    {
+        std::println(std::cerr, "{0}", image_size.error());
+        return 1;
+    }
     return 0;
 }
