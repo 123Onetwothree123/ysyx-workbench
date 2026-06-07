4fc1bb43a37f0946b7327ae2aac8103ffd9a7706
tracer-ysyx <tracer@ysyx.org>
2026-06-07 17:02:32 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  17:02:32 up  6:55,  1 user,  load average: 0.37, 0.38, 0.35

--- npc/src/Elaborate.scala
+++ npc/src/Elaborate.scala
@@ -3,5 +3,11 @@ import RV32I.RV32I
 
 object Elaborate extends App {
   val targetDir = args(args.indexOf("--target-dir") + 1)
-  emitVerilog(new RV32I, Array("--target-dir", targetDir, "--disable-verify"))
+  emitVerilog(new RV32I, Array("--target-dir", targetDir))
+  // firtool 生成了 include 但没拆出文件，补一个空壳
+  val pw = new java.io.PrintWriter(targetDir + "/layers-RV32I-Verification.sv")
+  pw.println("`ifndef layers_RV32I_Verification")
+  pw.println("  `define layers_RV32I_Verification")
+  pw.println("`endif")
+  pw.close()
 }
