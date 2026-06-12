c53bccd29acc9df2729af698452d50758461abc5
tracer-ysyx <tracer@ysyx.org>
2026-06-12 16:43:46 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  16:43:46 up  5:16,  1 user,  load average: 0.05, 0.17, 0.18

--- npc/csrc/ImageLoader.hpp
+++ npc/csrc/ImageLoader.hpp
@@ -8,7 +8,6 @@ class ImageLoader final
 {
 public:
     ImageLoader() = delete;
-    // SoC模式下镜像通过flash_read等DPI-C回调加载，不再需要Memory
     [[nodiscard]] static std::expected<std::size_t, std::string> LoadFromCLI(const CLIOptions &Options);
 };
 #endif
