5ba373c582c5d07ba8bcf687f71d0a5a7eec6315
100017119-Lin Junjie <alsttabblt@outlook.com>
2026-06-13 17:03:40 +0800

    verilator来骗来偷袭我一个19岁的老逼灯，OSOC的原文是： 上述char-test在输出字符A之后, 还输出一个换行符. 尝试仅仅输出字符A而不输出换行符, 你应该会观察到仿真过程连字符A都不输出了. 但如果每次输出一个字符之后都紧接着输出一个换行符, 打印出的信息将很难阅读.
    为了解决这个问题, 你只需要给verilator传递一个选项. 尝试根据你对这个问题的理解, 通过RTFM找到并添加这个选项. 如果你添加了正确的选项, 你将会看到即使上述程序仅输出单个字符A, 也能成功输出.
    
    Hint: PA讲义已经在好几处讨论过相关问题了. 如果你对此没有任何印象, 我们建议你重新仔细阅读讲义的每一处细节来查缺补漏.
    
    what？我查了下help命令，也确实去了官网查，也去过CSDN查，找到的命令是autoflush
    原文：--autoflush    Flush streams after all $displays
    但是这个东西是拿来刷新displays的，他解决不了write的东西
    反正就是我这台机器里面这个verilator5.048里，没有一个verilator命令行选项能直接解决$write的不换行来刷新缓冲区就可以正常的去打印输出的问题，当然我认为可能是我水平太次了，真的是丢人死了
    所以目前我的一个办法就不是按OSOC的文档来的，不是使用传verilator的命令选项的，而是使用Linux的标准缓冲stdbuf然后设置成0缓冲，就是无缓冲模式，然后强制输出的，这个问题我实在是不知道用什么命令解决了，verilator确实是看不太懂

--- npc/csrc/Test/char-test/cpp.hpp
+++ npc/csrc/Test/char-test/cpp.hpp
@@ -1,10 +1,10 @@
 #ifndef CPP_HPP
 #define CPP_HPP
-#define UART_BASE 0x100000000L
+#define UART_BASE 0x10000000L
 #define UART_TX   0
-void _start() {
+extern "C" void _start() {
   *(volatile char *)(UART_BASE + UART_TX) = 'A';
-  *(volatile char *)(UART_BASE + UART_TX) = '\n';
+  //*(volatile char *)(UART_BASE + UART_TX) = '\n';
   while (1);
 }
 #endif
--- npc/csrc/Test/char-test/main.cpp
+++ npc/csrc/Test/char-test/main.cpp
@@ -1,6 +1,1 @@
 #include "cpp.hpp"
-int main(int argc, char const *argv[])
-{
-    _start();
-    return 0;
-}
