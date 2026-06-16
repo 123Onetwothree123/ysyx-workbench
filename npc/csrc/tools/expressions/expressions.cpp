b0cb961a1b15512843010267e30b71b803367ac9
tracer-ysyx <tracer@ysyx.org>
2026-06-16 21:20:24 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  21:20:24 up  4:41,  1 user,  load average: 1.58, 1.12, 0.83

---
+++ npc/csrc/tools/expressions/expressions.cpp
@@ -0,0 +1,39 @@
+#include "expressions.hpp"
+#include "lexer.hpp"
+#include "parser.hpp"
+#include <string>
+std::expected<std::uint32_t, std::string> expressions::evaluate(std::string_view expression, const EvaluationContext &context)
+{
+    lexer lex{expression};
+    auto tokensResult{lex.ScanAll()};
+    if (!tokensResult)
+    {
+        return std::unexpected(tokensResult.error());
+    }
+    parser p{std::move(*tokensResult)};
+    auto astResult{p.parse()};
+    if (!astResult)
+    {
+        return std::unexpected(astResult.error());
+    }
+    try
+    {
+        return (*astResult)->Evaluate(context);
+    }
+    catch (const std::exception &e)
+    {
+        return std::unexpected(std::string{e.what()});
+    }
+}
+bool expressions::validate(std::string_view expression)
+{
+    lexer lex{expression};
+    auto tokensResult{lex.ScanAll()};
+    if (!tokensResult)
+    {
+        return false;
+    }
+    parser p{std::move(*tokensResult)};
+    auto astResult{p.parse()};
+    return static_cast<bool>(astResult);
+}
