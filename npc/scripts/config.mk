090f558ba0b68a1c392cd6fef5a21cd58dff84b5
tracer-ysyx <tracer@ysyx.org>
2026-06-17 20:20:21 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  20:20:21 up  1:28,  1 user,  load average: 0.34, 0.49, 0.49

--- npc/scripts/config.mk
+++ npc/scripts/config.mk
@@ -39,14 +39,10 @@ savedefconfig: $(CONF)
 
 .PHONY: menuconfig savedefconfig defconfig
 
-help:
-	@echo  '  menuconfig	  - Update current config utilising a menu based program'
-	@echo  '  savedefconfig   - Save current config as configs/defconfig (minimal config)'
-
 distclean: clean
 	-@rm -rf $(rm-distclean)
 
-.PHONY: help distclean
+.PHONY: distclean
 
 define call_fixdep
 	@$(FIXDEP) $(1) $(2) unused > $(1).tmp
