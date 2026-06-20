ed50dc8574f804301ac700fe145cb62368c24e14
tracer-ysyx <tracer@ysyx.org>
2026-06-20 09:49:27 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  09:49:27 up 5 min,  1 user,  load average: 0.37, 0.61, 0.28

--- npc/include/generated/autoconf.h
+++ npc/include/generated/autoconf.h
@@ -5,9 +5,10 @@
  *
  */
 #define CONFIG_BUILD_THREADS 0
-#define CONFIG_CC_CLANG 1
+#define CONFIG_CC_LTO 1
 #define CONFIG_SDB 1
 #define CONFIG_CC_OPT "-O3"
-#define CONFIG_CC "clang++"
+#define CONFIG_CC_GPP 1
+#define CONFIG_CC "g++"
 #define CONFIG_CC_O3 1
 #define CONFIG_SIM_THREADS 1
