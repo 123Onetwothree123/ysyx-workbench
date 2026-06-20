748fe3cac1ec4e73680759f9f8a5da878fb13efa
tracer-ysyx <tracer@ysyx.org>
2026-06-20 14:36:05 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  14:36:05 up  1:16,  1 user,  load average: 0.84, 1.00, 0.89

--- npc/csrc/trace/disasm.cpp
+++ npc/csrc/trace/disasm.cpp
@@ -1,6 +1,5 @@
 module;
 #include <capstone/capstone.h>
-#include <assert.h>
 module npc.trace.disasm;
 static csh handle{};
 void init_disasm()
@@ -17,7 +16,7 @@ void disassemble(char *str, int size, std::uint64_t pc, std::uint8_t *code, int 
 {
     cs_insn *insn{nullptr};
     const auto count{cs_disasm(handle, code, static_cast<size_t>(nbyte), pc, 0, &insn)};
-    assert(count == 1);
+    
     const auto ret{std::snprintf(str, size, "%s", insn->mnemonic)};
     if (insn->op_str[0] != '\0')
     {
--- npc/csrc/ysyxSoC/ysyxSoC.cpp
+++ npc/csrc/ysyxSoC/ysyxSoC.cpp
@@ -1,5 +1,4 @@
 module;
-#include <cassert>
 module npc.ysyxSoC;
 #ifndef CONFIG_MBASE
 #define CONFIG_MBASE 0x20000000u
@@ -7,11 +6,10 @@ module npc.ysyxSoC;
 std::vector<std::uint8_t> mrom;
 extern "C" void flash_read(std::int32_t addr, std::int32_t *data)
 {
-    assert(0);
+    std::unreachable();
 }
 extern "C" void mrom_read(std::int32_t addr, std::int32_t *data)
 {
-    assert(data != nullptr);
     std::uint32_t offset{static_cast<std::uint32_t>(addr) - static_cast<std::uint32_t>(CONFIG_MBASE)}; // 脑残编译器，这里auto会解读成ui32，他妈的那每次auto写了后，还得检查一遍，这和没写auto有什么区别，他妈的
     if (offset + 3 >= mrom.size())
     {
