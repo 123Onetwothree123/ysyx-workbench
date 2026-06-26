c306bb1b23d5f03e08edd07d2bc0836222702958
tracer-ysyx <tracer@ysyx.org>
2026-06-26 14:16:16 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  14:16:16 up  2:58,  1 user,  load average: 0.15, 0.85, 1.47

--- npc/csrc/ysyxSoC/ysyxSoC.cpp
+++ npc/csrc/ysyxSoC/ysyxSoC.cpp
@@ -11,11 +11,13 @@ extern "C" void flash_read(std::int32_t addr, std::int32_t *data)
 extern "C" void mrom_read(std::int32_t addr, std::int32_t *data)
 {
     std::uint32_t offset{static_cast<std::uint32_t>(addr) - static_cast<std::uint32_t>(CONFIG_MBASE)}; // 脑残编译器，这里auto会解读成ui32，他妈的那每次auto写了后，还得检查一遍，这和没写auto有什么区别，他妈的
-    if (offset + 3 >= mrom.size())
+    std::uint32_t result{0};
+    for (int i = 0; i < 4; i++)
     {
-        std::println("超C++的mrom的范围了");
-        *data = 0;
-        return;
+        if (offset + i < mrom.size())
+        {
+            result |= static_cast<std::uint32_t>(mrom[offset + i]) << (8 * i);
+        }
     }
-    *data = static_cast<std::int32_t>(static_cast<std::uint32_t>(mrom[offset]) | (static_cast<std::uint32_t>(mrom[offset + 1]) << 8) | (static_cast<std::uint32_t>(mrom[offset + 2]) << 16) | (static_cast<std::uint32_t>(mrom[offset + 3]) << 24));
+    *data = static_cast<std::int32_t>(result);
 }
