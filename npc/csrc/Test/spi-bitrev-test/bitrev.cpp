ec05efafdb6e68d582ba6cc1e42681e8d597c86f
tracer-ysyx <tracer@ysyx.org>
2026-07-04 11:38:21 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  11:38:21 up  2:13,  1 user,  load average: 0.33, 0.43, 0.37

---
+++ npc/csrc/Test/spi-bitrev-test/bitrev.cpp
@@ -0,0 +1,25 @@
+#include <klib-macros.h>
+#include <klib.h>
+#include "bitrev.hpp"
+#include <cstdint>
+int main(const char *args)
+{
+    volatile std::uint32_t *spi = (volatile std::uint32_t *)BITREV_BASE;
+    spi[SPI_DIVIDER] = 1;
+    spi[SPI_SS] = BITREV_SS;
+    spi[SPI_TX_0] = 0xC2;
+    spi[SPI_CTRL] = CTRL_CHAR_LEN | CTRL_GO | CTRL_TX_NEG | CTRL_LSB;
+    // 等硬件自动清零
+    while (spi[SPI_CTRL] & CTRL_GO)
+        ;
+    std::uint32_t rx = spi[SPI_RX_0] & 0xFF;
+    if (rx == 0x43)
+    {
+        putstr("PASS\n");
+    }
+    else
+    {
+        putstr("FAIL\n");
+    }
+    return 0;
+}
