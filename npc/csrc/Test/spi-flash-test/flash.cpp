81717862717100fc22a60474a220b61f8ae55470
tracer-ysyx <tracer@ysyx.org>
2026-07-04 13:08:06 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  13:08:06 up  3:43,  1 user,  load average: 0.24, 0.32, 0.43

--- npc/csrc/Test/spi-flash-test/flash.cpp
+++ npc/csrc/Test/spi-flash-test/flash.cpp
@@ -1,13 +1,21 @@
 #include "flash.hpp"
 #include <klib-macros.h>
 #include <klib.h>
+// 位翻转，他妈的flash模型会逆序接收
+constexpr std::uint8_t bitrev(std::uint8_t b)
+{
+    b = ((b & 0xF0) >> 4) | ((b & 0x0F) << 4);
+    b = ((b & 0xCC) >> 2) | ((b & 0x33) << 2);
+    b = ((b & 0xAA) >> 1) | ((b & 0x55) << 1);
+    return b;
+}
 std::uint32_t flash_read(std::uint32_t addr)
 {
-    volatile auto *spi {reinterpret_cast<volatile uint32_t *>(SPI_BASE)};
+    volatile auto *spi{reinterpret_cast<volatile uint32_t *>(SPI_BASE)};
     spi[SPI_DIVIDER] = 1;
     spi[SPI_SS] = FLASH_SS;
     spi[SPI_CTRL] = CTRL_CHAR_LEN | CTRL_TX_NEG | CTRL_LSB;
-    spi[SPI_TX_0] = (addr << 8) | FLASH_CMD;
+    spi[SPI_TX_0] = (addr << 8) | bitrev(FLASH_CMD);
     spi[SPI_CTRL] = CTRL_CHAR_LEN | CTRL_GO | CTRL_TX_NEG | CTRL_LSB;
     while (spi[SPI_CTRL] & CTRL_GO)
         ;
