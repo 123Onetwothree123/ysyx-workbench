ebf389404f5f2a978172a0c3a06c35a5aff5b1ac
tracer-ysyx <tracer@ysyx.org>
2026-07-06 19:11:29 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  19:11:29 up  9:30,  1 user,  load average: 1.25, 1.32, 1.44

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -47,8 +47,12 @@ int main(int argc, char const *argv[])
         auto pc = static_cast<uint32_t>(dut->debug_pc);
         if (pc != last_pc) {
             last_pc = pc;
-            if (s < 200000)
-                std::println("[{}]pc=0x{:08x}", s, pc);
+            if (pc == 0x300000c8 || pc == 0x300000d0) {
+                auto a0 = dut.ReadGPR(10);
+                auto t0 = dut.ReadGPR(5);
+                auto t1 = dut.ReadGPR(6);
+                std::println("[{}]pc=0x{:08x} a0=0x{:08x} t0=0x{:08x} t1=0x{:08x}", s, pc, a0.value_or(0), t0.value_or(0), t1.value_or(0));
+            }
         }
         if (dut->trap_valid)
         {
