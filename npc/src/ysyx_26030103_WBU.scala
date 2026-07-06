2f1bf3b54fa1499454a8e6e7489e745a9cc56e79
tracer-ysyx <tracer@ysyx.org>
2026-07-06 19:33:10 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  19:33:10 up  9:51,  1 user,  load average: 1.87, 1.50, 1.35

--- npc/src/ysyx_26030103_WBU.scala
+++ npc/src/ysyx_26030103_WBU.scala
@@ -11,21 +11,19 @@ class ysyx_26030103_WBU extends Module {
     val wdata = Output(UInt(32.W))
   })
   io.in.ready := true.B
-  io.WriteSELECT := io.in.bits.Rd
-  io.WriteEN := io.in.fire && io.in.bits.RegisterWrite
-  io.wdata := 0.U(32.W)
-  switch(io.in.bits.WBSelect) {
-    is("b00".U) {
-      io.wdata := io.in.bits.ALUResult
-    }
-    is("b01".U) {
-      io.wdata := io.in.bits.LoadData
-    }
-    is("b10".U) {
-      io.wdata := io.in.bits.snpc
-    }
-    is("b11".U) {
-      io.wdata := io.in.bits.CSRReadData
+  val wen_reg = RegNext(io.in.fire && io.in.bits.RegisterWrite, false.B)
+  val wdata_reg = RegNext(0.U(32.W))
+  val waddr_reg = RegNext(0.U(5.W))
+  when(io.in.fire) {
+    waddr_reg := io.in.bits.Rd
+    switch(io.in.bits.WBSelect) {
+      is("b00".U) { wdata_reg := io.in.bits.ALUResult }
+      is("b01".U) { wdata_reg := io.in.bits.LoadData }
+      is("b10".U) { wdata_reg := io.in.bits.snpc }
+      is("b11".U) { wdata_reg := io.in.bits.CSRReadData }
     }
   }
+  io.WriteEN := wen_reg
+  io.wdata := wdata_reg
+  io.WriteSELECT := waddr_reg
 }
