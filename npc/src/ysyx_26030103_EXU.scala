f5656c541aea0d070c48c0abcc755191f13ecfef
tracer-ysyx <tracer@ysyx.org>
2026-07-06 16:21:35 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  16:21:35 up  6:40,  1 user,  load average: 1.36, 1.70, 1.62

--- npc/src/ysyx_26030103_EXU.scala
+++ npc/src/ysyx_26030103_EXU.scala
@@ -95,7 +95,7 @@ class ysyx_26030103_EXU extends Module {
   }.otherwise {
     io.RedirectTarget := BranchTarget
   }
-  io.ExceptionTaken := InstructionExecutionDone && CSRUnit.io.ExceptionTaken && !ActiveInstruction.IsEbreak
+  io.ExceptionTaken := InstructionExecutionDone && CSRUnit.io.ExceptionTaken
   io.ExceptionTarget := CSRUnit.io.ExceptionTarget
   io.MemoryWrite := ActiveInstruction.MemoryWrite
   io.WidthSelect := ActiveInstruction.WidthSelect
