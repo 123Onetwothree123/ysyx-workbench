99f7dc3396d25a6d20481325b5bf09698103389a
100017119-Lin Junjie <alsttabblt@outlook.com>
2026-07-07 11:39:57 +0800

    准备重构了，干了两天了，吃了我四十块钱token，结果AI也解决不了问题，我得手动重构代码了，他妈的，烦死了，当初就不该选scala，就应该继续用HDL的

---
+++ npc/csrc/Test/psram-mem-test/main.cpp
@@ -0,0 +1,16 @@
+#include "PSRAMMemTest.hpp"
+#include <stdio.h>
+#include <am.h>
+int main(const char *)
+{
+    const auto begin{reinterpret_cast<std::uintptr_t>(0x80000000U)};
+    const auto length{static_cast<std::size_t>(0x1000)};
+    const auto result{psram_mem_test(begin, length)};
+    if (!result)
+    {
+        printf("失败了 0x%08x\n", result.error());
+        halt(1);
+    }
+    printf("测试通过了");
+    return 0;
+}
