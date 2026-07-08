ee0c422412361279f97dc37bec30912d830ab247
tracer-ysyx <tracer@ysyx.org>
2026-07-08 10:51:19 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  10:51:19 up  1:31,  1 user,  load average: 0.55, 0.38, 0.27

--- ysyxSoC/perip/psram/efabless/EF_PSRAM_CTRL_wb.v
+++ ysyxSoC/perip/psram/efabless/EF_PSRAM_CTRL_wb.v
@@ -180,7 +180,7 @@ module EF_PSRAM_CTRL_wb (
     assign sck  = init_active ? init_sck : (wb_we ? mw_sck  : mr_sck);
     assign ce_n = init_active ? 1'b0     : (wb_we ? mw_ce_n : mr_ce_n);
     wire [7:0] CMD_35H = 8'h35;
-    aassign dout = init_active ? {3'b0, CMD_35H[7 - init_cnt]} : (wb_we ? mw_dout : mr_dout);
+    assign dout = init_active ? {3'b0, CMD_35H[7 - init_cnt]} : (wb_we ? mw_dout : mr_dout);
     assign douten = init_active ? 4'b1111 : (wb_we ? {4{mw_doe}} : {4{mr_doe}});
 
     assign mw_din = din;
