11dcd27d3b7d083973a76653c26f136df4b1c0cb
tracer-ysyx <tracer@ysyx.org>
2026-07-11 16:49:26 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  16:49:26 up  3:38,  1 user,  load average: 1.83, 1.04, 0.63

--- ysyxSoC/src/device/SDRAM.scala
+++ ysyxSoC/src/device/SDRAM.scala
@@ -13,21 +13,25 @@ import freechips.rocketchip.util._
 class SDRAMIO extends Bundle {
   val clk = Output(Bool())
   val cke = Output(Bool())
-  val cs  = Output(Bool())
+  val cs = Output(Bool())
   val ras = Output(Bool())
   val cas = Output(Bool())
-  val we  = Output(Bool())
-  val a   = Output(UInt(13.W))
-  val ba  = Output(UInt(2.W))
+  val we = Output(Bool())
+  val a = Output(UInt(13.W))
+  val ba = Output(UInt(2.W))
   val dqm = Output(UInt(2.W))
-  val dq  = Analog(16.W)
+  val dq = Analog(16.W)
 }
 
 class sdram_top_axi extends BlackBox {
   val io = IO(new Bundle {
     val clock = Input(Clock())
     val reset = Input(Bool())
-    val in = Flipped(new AXI4Bundle(AXI4BundleParameters(addrBits = 32, dataBits = 32, idBits = 4)))
+    val in = Flipped(
+      new AXI4Bundle(
+        AXI4BundleParameters(addrBits = 32, dataBits = 32, idBits = 4)
+      )
+    )
     val sdram = new SDRAMIO
   })
 }
@@ -36,7 +40,8 @@ class sdram_top_apb extends BlackBox {
   val io = IO(new Bundle {
     val clock = Input(Clock())
     val reset = Input(Bool())
-    val in = Flipped(new APBBundle(APBBundleParameters(addrBits = 32, dataBits = 32)))
+    val in =
+      Flipped(new APBBundle(APBBundleParameters(addrBits = 32, dataBits = 32)))
     val sdram = new SDRAMIO
   })
 }
@@ -47,108 +52,131 @@ class sdram extends BlackBox {
 
 class sdramChisel extends RawModule {
   val io = IO(Flipped(new SDRAMIO))
-  val output=Wire(UInt(16.W))
-  val en=Wire(Bool())
-  val input=TriStateInBuf(io.dq, output, en)
-  val Command_NO_OPERATION=io.cs || (io.ras && io.cas && io.we)
-  val Command_ACTIVE=!io.cs && !io.ras &&  io.cas &&  io.we
-  val Command_READ=!io.cs &&  io.ras && !io.cas &&  io.we
-  val Command_WRITE=!io.cs &&  io.ras && !io.cas && !io.we
-  val Command_BURST_TERMINATE=!io.cs&&io.ras&&io.cas&&!io.we
-  val Command_PRECHAREG=!io.cs&&!io.ras&&io.cas&&!io.we
-  val Command_AUTO_REFRESH=!io.cs&&!io.ras&&!io.cas&&io.we
-  val Command_LOAD_MODE_REGISTER=!io.cs&&!io.ras&&!io.cas&&!io.we
-  val memory=Mem(4,Mem(8192,Vec(512,UInt(16.W))))
-  val ROWBuffer=Reg(Vec(512, UInt(16.W)))//行缓冲
-  val ModeRegister=RegInit(0x20.U(13.W))
-  val MR_Burst_Length  = MuxLookup(ModeRegister(2, 0), 1.U)(
-  Seq(0.U -> 1.U, 1.U -> 2.U, 2.U -> 4.U, 3.U -> 8.U, 7.U -> 256.U))
-  val MR_CAS_Latency=ModeRegister(6,4)
-  val MR_Burst_Type=ModeRegister(3)
-  val MR_Write_Burst_Mode=ModeRegister(9)
-  val state_idle::state_active::state_read::state_read_data::state_write::state_write_data=Enum(6)
-  val state=RegInit(state_idle)
-  val ActiveMemoryBank=Reg(UInt(2.W))//memory bank中文存储体
-  val ActiveROWAddress_In_A_MemoryBank=Reg(UInt(13.W))
-  val ActiveColumnAddress_In_A_MemoryBank=Reg(UInt(9.W))
-  val BurstCounter=Reg(UInt(8.W))//SDRAM真的牛逼，居然所有读写操作都是属于突发情况
-  output:=0.U
-  en:=false.B
-  switch(state){
-    is(state_idle){
-      when(Command_ACTIVE){
-        ActiveMemoryBank:=io.ba
-        ActiveROWAddress_In_A_MemoryBank:=io.a
-        state:=state_active
+  val output = Wire(UInt(16.W))
+  val en = Wire(Bool())
+  val input = TriStateInBuf(io.dq, output, en)
+
+  withClockAndReset(io.clk.asClock, false.B.asAsyncReset) {
+  val Command_NO_OPERATION = Wire(Bool())
+  val Command_ACTIVE = Wire(Bool())
+  val Command_READ = Wire(Bool())
+  val Command_WRITE = Wire(Bool())
+  val Command_BURST_TERMINATE = Wire(Bool())
+  val Command_PRECHAREG = Wire(Bool())
+  val Command_AUTO_REFRESH = Wire(Bool())
+  val Command_LOAD_MODE_REGISTER = Wire(Bool())
+
+  Command_NO_OPERATION := io.cs || (io.ras && io.cas && io.we)
+  Command_ACTIVE := (~io.cs) && (~io.ras) && io.cas && io.we
+  Command_READ := (~io.cs) && io.ras && (~io.cas) && io.we
+  Command_WRITE := (~io.cs) && io.ras && (~io.cas) && (~io.we)
+  Command_BURST_TERMINATE := (~io.cs) && io.ras && io.cas && (~io.we)
+  Command_PRECHAREG := (~io.cs) && (~io.ras) && io.cas && (~io.we)
+  Command_AUTO_REFRESH := (~io.cs) && (~io.ras) && (~io.cas) && io.we
+  Command_LOAD_MODE_REGISTER := (~io.cs) && (~io.ras) && (~io.cas) && (~io.we)
+  val memory = Seq.fill(4)(Mem(8192, Vec(512, UInt(16.W))))
+  val ROWBuffer = Reg(Vec(512, UInt(16.W))) // 行缓冲
+  val ModeRegister = RegInit(0x20.U(13.W))
+  val MR_Burst_Length = MuxLookup(ModeRegister(2, 0), 1.U)(
+    Seq(0.U -> 1.U, 1.U -> 2.U, 2.U -> 4.U, 3.U -> 8.U, 7.U -> 256.U)
+  )
+  val MR_CAS_Latency = ModeRegister(6, 4)
+  val MR_Burst_Type = ModeRegister(3)
+  val MR_Write_Burst_Mode = ModeRegister(9)
+  val state_idle :: state_active :: state_read :: state_read_data :: state_write :: state_write_data :: Nil =
+    Enum(6)
+  val state = RegInit(state_idle)
+  val ActiveMemoryBank = Reg(UInt(2.W)) // memory bank中文存储体
+  val ActiveROWAddress_In_A_MemoryBank = Reg(UInt(13.W))
+  val ActiveColumnAddress_In_A_MemoryBank = Reg(UInt(9.W))
+  val BurstCounter = Reg(UInt(8.W)) // SDRAM真的牛逼，居然所有读写操作都是属于突发情况
+  output := 0.U
+  en := false.B
+  switch(state) {
+    is(state_idle) {
+      when(Command_ACTIVE) {
+        ActiveMemoryBank := io.ba
+        ActiveROWAddress_In_A_MemoryBank := io.a
+        state := state_active
       }
-      when(Command_LOAD_MODE_REGISTER){
-        ModeRegister:=io.a
-        state:=state_idle
+      when(Command_LOAD_MODE_REGISTER) {
+        ModeRegister := io.a
+        state := state_idle
       }
-      when(Command_READ){
-        ActiveColumnAddress_In_A_MemoryBank:=io.a(8,0)
-        BurstCounter:=0.U
-        state:=state_read
+      when(Command_READ) {
+        ActiveColumnAddress_In_A_MemoryBank := io.a(8, 0)
+        BurstCounter := 0.U
+        state := state_read
       }
-      when(Command_WRITE){
-        ActiveColumnAddress_In_A_MemoryBank:=io.a(8,0)
-        BurstCounter:=0.U
-        state:=state_write
+      when(Command_WRITE) {
+        ActiveColumnAddress_In_A_MemoryBank := io.a(8, 0)
+        BurstCounter := 0.U
+        state := state_write
       }
-      when(Command_PRECHAREG){
-        //memory(ActiveMemoryBank)(ActiveROWAddress_In_A_MemoryBank) := ROWBuffer
-        state:=state_idle
+      when(Command_PRECHAREG) {
+        // memory(ActiveMemoryBank)(ActiveROWAddress_In_A_MemoryBank) := ROWBuffer
+        state := state_idle
       }
-      when(Command_AUTO_REFRESH){
-        state:=state_idle
-      }
-    }
-    is(state_active){
-      ROWBuffer:=memory(ActiveMemoryBank)(ActiveROWAddress_In_A_MemoryBank)
-      state:=state_idle
-    }
-    is(state_read){
-      when(BurstCounter<(MR_CAS_Latency-1.U)){
-        BurstCounter:=BurstCounter+1.U
-      }.otherwise{
-        BurstCounter:=0.U
-        state:=state_read_data
-      }
-    }
-    is(state_read_data){
-      en=true.B
-      output:=ROWBuffer(ActiveColumnAddress_In_A_MemoryBank+BurstCounter)
-      when(BurstCounter===(MR_Burst_Length-1.U)){
-        state:=state_idle
-      }.otherwise{
-        BurstCounter:=BurstCounter+1.U
+      when(Command_AUTO_REFRESH) {
+        state := state_idle
       }
     }
-    is(state_write){
-      state:=state_write_data
+    is(state_active) {
+      ROWBuffer := MuxLookup(ActiveMemoryBank, memory(0)(ActiveROWAddress_In_A_MemoryBank))(
+        (1 until 4).map(i => i.U -> memory(i)(ActiveROWAddress_In_A_MemoryBank)))
+      state := state_idle
     }
-    is(state_write_data){
-      ROWBuffer(ActiveColumnAddress_In_A_MemoryBank+BurstCounter):=input
-      when(MR_Write_Burst_Mode||BurstCounter===MR_Burst_Length-1.U){
-        state:=state_idle//single write只要一拍
-      }.otherwise{
-        BurstCounter:=BurstCounter+1.U
+    is(state_read) {
+      when(BurstCounter < (MR_CAS_Latency - 1.U)) {
+        BurstCounter := BurstCounter + 1.U
+      }.otherwise {
+        BurstCounter := 0.U
+        state := state_read_data
       }
     }
+    is(state_read_data) {
+      en := true.B
+      output := ROWBuffer(ActiveColumnAddress_In_A_MemoryBank + BurstCounter)
+      when(BurstCounter === (MR_Burst_Length - 1.U)) {
+        state := state_idle
+      }.otherwise {
+        BurstCounter := BurstCounter + 1.U
+      }
+    }
+    is(state_write) {
+      state := state_write_data
+    }
+    is(state_write_data) {
+      ROWBuffer(ActiveColumnAddress_In_A_MemoryBank + BurstCounter) := input
+      when(MR_Write_Burst_Mode || BurstCounter === MR_Burst_Length - 1.U) {
+        state := state_idle // single write只要一拍
+      }.otherwise {
+        BurstCounter := BurstCounter + 1.U
+      }
+  }
   }
 }
+}
 
-class AXI4SDRAM(address: Seq[AddressSet])(implicit p: Parameters) extends LazyModule {
+class AXI4SDRAM(address: Seq[AddressSet])(implicit p: Parameters)
+    extends LazyModule {
   val beatBytes = 4
-  val node = AXI4SlaveNode(Seq(AXI4SlavePortParameters(
-    Seq(AXI4SlaveParameters(
-        address       = address,
-        executable    = true,
-        supportsWrite = TransferSizes(1, beatBytes),
-        supportsRead  = TransferSizes(1, beatBytes),
-        interleavedId = Some(0))
-    ),
-    beatBytes  = beatBytes)))
+  val node = AXI4SlaveNode(
+    Seq(
+      AXI4SlavePortParameters(
+        Seq(
+          AXI4SlaveParameters(
+            address = address,
+            executable = true,
+            supportsWrite = TransferSizes(1, beatBytes),
+            supportsRead = TransferSizes(1, beatBytes),
+            interleavedId = Some(0)
+          )
+        ),
+        beatBytes = beatBytes
+      )
+    )
+  )
 
   lazy val module = new Impl
   class Impl extends LazyModuleImp(this) {
@@ -163,14 +191,23 @@ class AXI4SDRAM(address: Seq[AddressSet])(implicit p: Parameters) extends LazyMo
   }
 }
 
-class APBSDRAM(address: Seq[AddressSet])(implicit p: Parameters) extends LazyModule {
-  val node = APBSlaveNode(Seq(APBSlavePortParameters(
-    Seq(APBSlaveParameters(
-      address       = address,
-      executable    = true,
-      supportsRead  = true,
-      supportsWrite = true)),
-    beatBytes  = 4)))
+class APBSDRAM(address: Seq[AddressSet])(implicit p: Parameters)
+    extends LazyModule {
+  val node = APBSlaveNode(
+    Seq(
+      APBSlavePortParameters(
+        Seq(
+          APBSlaveParameters(
+            address = address,
+            executable = true,
+            supportsRead = true,
+            supportsWrite = true
+          )
+        ),
+        beatBytes = 4
+      )
+    )
+  )
 
   lazy val module = new Impl
   class Impl extends LazyModuleImp(this) {
