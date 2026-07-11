916aa6d9ad464cc41d8422acb936f7f5182c1819
tracer-ysyx <tracer@ysyx.org>
2026-07-11 19:15:24 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  19:15:24 up  6:04,  1 user,  load average: 0.29, 0.52, 0.51

--- ysyxSoC/src/device/SDRAM.scala
+++ ysyxSoC/src/device/SDRAM.scala
@@ -57,115 +57,137 @@ class sdramChisel extends RawModule {
   val input = TriStateInBuf(io.dq, output, en)
 
   withClockAndReset(io.clk.asClock, false.B.asAsyncReset) {
-  val Command_NO_OPERATION = Wire(Bool())
-  val Command_ACTIVE = Wire(Bool())
-  val Command_READ = Wire(Bool())
-  val Command_WRITE = Wire(Bool())
-  val Command_BURST_TERMINATE = Wire(Bool())
-  val Command_PRECHAREG = Wire(Bool())
-  val Command_AUTO_REFRESH = Wire(Bool())
-  val Command_LOAD_MODE_REGISTER = Wire(Bool())
+    val Command_NO_OPERATION = Wire(Bool())
+    val Command_ACTIVE = Wire(Bool())
+    val Command_READ = Wire(Bool())
+    val Command_WRITE = Wire(Bool())
+    val Command_BURST_TERMINATE = Wire(Bool())
+    val Command_PRECHAREG = Wire(Bool())
+    val Command_AUTO_REFRESH = Wire(Bool())
+    val Command_LOAD_MODE_REGISTER = Wire(Bool())
 
-  Command_NO_OPERATION := io.cs || (io.ras && io.cas && io.we)
-  Command_ACTIVE := (~io.cs) && (~io.ras) && io.cas && io.we
-  Command_READ := (~io.cs) && io.ras && (~io.cas) && io.we
-  Command_WRITE := (~io.cs) && io.ras && (~io.cas) && (~io.we)
-  Command_BURST_TERMINATE := (~io.cs) && io.ras && io.cas && (~io.we)
-  Command_PRECHAREG := (~io.cs) && (~io.ras) && io.cas && (~io.we)
-  Command_AUTO_REFRESH := (~io.cs) && (~io.ras) && (~io.cas) && io.we
-  Command_LOAD_MODE_REGISTER := (~io.cs) && (~io.ras) && (~io.cas) && (~io.we)
-  val memory = Seq.fill(4)(Mem(8192, Vec(512, UInt(16.W))))
-  val ROWBuffer = Reg(Vec(512, UInt(16.W))) // 行缓冲
-  val ModeRegister = RegInit(0x20.U(13.W))
-  val MR_Burst_Length = MuxLookup(ModeRegister(2, 0), 1.U)(
-    Seq(0.U -> 1.U, 1.U -> 2.U, 2.U -> 4.U, 3.U -> 8.U, 7.U -> 256.U)
-  )
-  val MR_CAS_Latency = ModeRegister(6, 4)
-  val MR_Burst_Type = ModeRegister(3)
-  val MR_Write_Burst_Mode = ModeRegister(9)
-  val state_idle :: state_active :: state_read :: state_read_data :: state_write :: state_write_data :: Nil =
-    Enum(6)
-  val state = RegInit(state_idle)
-  val ActiveMemoryBank = Reg(UInt(2.W)) // memory bank中文存储体
-  val ActiveROWAddress_In_A_MemoryBank = Reg(UInt(13.W))
-  val ActiveColumnAddress_In_A_MemoryBank = Reg(UInt(9.W))
-  val BurstCounter = Reg(UInt(8.W)) // SDRAM真的牛逼，居然所有读写操作都是属于突发情况
-  output := 0.U
-  en := false.B
-  switch(state) {
-    is(state_idle) {
-      when(Command_ACTIVE) {
-        ActiveMemoryBank := io.ba
-        ActiveROWAddress_In_A_MemoryBank := io.a
-        state := state_active
+    Command_NO_OPERATION := io.cs || (io.ras && io.cas && io.we)
+    Command_ACTIVE := (~io.cs) && (~io.ras) && io.cas && io.we
+    Command_READ := (~io.cs) && io.ras && (~io.cas) && io.we
+    Command_WRITE := (~io.cs) && io.ras && (~io.cas) && (~io.we)
+    Command_BURST_TERMINATE := (~io.cs) && io.ras && io.cas && (~io.we)
+    Command_PRECHAREG := (~io.cs) && (~io.ras) && io.cas && (~io.we)
+    Command_AUTO_REFRESH := (~io.cs) && (~io.ras) && (~io.cas) && io.we
+    Command_LOAD_MODE_REGISTER := (~io.cs) && (~io.ras) && (~io.cas) && (~io.we)
+    val memory = Seq.fill(4)(Mem(8192, Vec(512, UInt(16.W))))
+    val ROWBuffer = Reg(Vec(512, UInt(16.W))) // 行缓冲
+    val ModeRegister = RegInit(0x20.U(13.W))
+    val MR_Burst_Length = MuxLookup(ModeRegister(2, 0), 1.U)(
+      Seq(0.U -> 1.U, 1.U -> 2.U, 2.U -> 4.U, 3.U -> 8.U, 7.U -> 256.U)
+    )
+    val MR_CAS_Latency = ModeRegister(6, 4)
+    val MR_Burst_Type = ModeRegister(3)
+    val MR_Write_Burst_Mode = ModeRegister(9)
+    val state_idle :: state_active :: state_read :: state_read_data :: state_write :: state_write_data :: Nil =
+      Enum(6)
+    val state = RegInit(state_idle)
+    val ActiveMemoryBank = Reg(UInt(2.W)) // memory bank中文存储体
+    val ActiveROWAddress_In_A_MemoryBank = Reg(UInt(13.W))
+    val ActiveColumnAddress_In_A_MemoryBank = Reg(UInt(9.W))
+    val BurstCounter = Reg(UInt(8.W)) // SDRAM真的牛逼，居然所有读写操作都是属于突发情况
+    output := 0.U
+    en := false.B
+    switch(state) {
+      is(state_idle) {
+        when(Command_ACTIVE) {
+          ActiveMemoryBank := io.ba
+          ActiveROWAddress_In_A_MemoryBank := io.a
+          state := state_active
+        }
+        when(Command_LOAD_MODE_REGISTER) {
+          ModeRegister := io.a
+          state := state_idle
+        }
+        when(Command_READ) {
+          ActiveColumnAddress_In_A_MemoryBank := io.a(8, 0)
+          BurstCounter := 0.U
+          state := state_read
+        }
+        when(Command_WRITE) {
+          ActiveColumnAddress_In_A_MemoryBank := io.a(8, 0)
+          val col0 = io.a(8, 0)
+          val old0 = ROWBuffer(col0)
+          ROWBuffer(col0) := Cat(
+            Mux(!io.dqm(1), input(15, 8), old0(15, 8)),
+            Mux(!io.dqm(0), input(7, 0), old0(7, 0))
+          )
+          BurstCounter := 1.U
+          when(MR_Write_Burst_Mode || MR_Burst_Length === 1.U) {
+            state := state_idle
+          }.otherwise {
+            state := state_write_data
+          }
+        }
+        when(Command_PRECHAREG) {
+          memory(0)(ActiveROWAddress_In_A_MemoryBank) := ROWBuffer
+          when(ActiveMemoryBank === 1.U) {
+            memory(1)(ActiveROWAddress_In_A_MemoryBank) := ROWBuffer
+          }
+          when(ActiveMemoryBank === 2.U) {
+            memory(2)(ActiveROWAddress_In_A_MemoryBank) := ROWBuffer
+          }
+          when(ActiveMemoryBank === 3.U) {
+            memory(3)(ActiveROWAddress_In_A_MemoryBank) := ROWBuffer
+          }
+          state := state_idle
+        }
+        when(Command_AUTO_REFRESH) {
+          state := state_idle
+        }
       }
-      when(Command_LOAD_MODE_REGISTER) {
-        ModeRegister := io.a
+      is(state_active) {
+        ROWBuffer := memory(0)(ActiveROWAddress_In_A_MemoryBank)
+        when(ActiveMemoryBank === 1.U) {
+          ROWBuffer := memory(1)(ActiveROWAddress_In_A_MemoryBank)
+        }
+        when(ActiveMemoryBank === 2.U) {
+          ROWBuffer := memory(2)(ActiveROWAddress_In_A_MemoryBank)
+        }
+        when(ActiveMemoryBank === 3.U) {
+          ROWBuffer := memory(3)(ActiveROWAddress_In_A_MemoryBank)
+        }
         state := state_idle
       }
-      when(Command_READ) {
-        ActiveColumnAddress_In_A_MemoryBank := io.a(8, 0)
-        BurstCounter := 0.U
-        state := state_read
+      is(state_read) {
+        when(BurstCounter < (MR_CAS_Latency - 1.U)) {
+          BurstCounter := BurstCounter + 1.U
+        }.otherwise {
+          BurstCounter := 0.U
+          state := state_read_data
+        }
       }
-      when(Command_WRITE) {
-        ActiveColumnAddress_In_A_MemoryBank := io.a(8, 0)
-        BurstCounter := 0.U
-        state := state_write
+      is(state_read_data) {
+        en := true.B
+        output := ROWBuffer(ActiveColumnAddress_In_A_MemoryBank + BurstCounter)
+        when(BurstCounter === (MR_Burst_Length - 1.U)) {
+          state := state_idle
+        }.otherwise {
+          BurstCounter := BurstCounter + 1.U
+        }
       }
-      when(Command_PRECHAREG) {
-        memory(0)(ActiveROWAddress_In_A_MemoryBank) := ROWBuffer
-        when(ActiveMemoryBank === 1.U) { memory(1)(ActiveROWAddress_In_A_MemoryBank) := ROWBuffer }
-        when(ActiveMemoryBank === 2.U) { memory(2)(ActiveROWAddress_In_A_MemoryBank) := ROWBuffer }
-        when(ActiveMemoryBank === 3.U) { memory(3)(ActiveROWAddress_In_A_MemoryBank) := ROWBuffer }
-        state := state_idle
+      is(state_write) {
+        state := state_write_data
       }
-      when(Command_AUTO_REFRESH) {
-        state := state_idle
-      }
-    }
-    is(state_active) {
-      ROWBuffer := memory(0)(ActiveROWAddress_In_A_MemoryBank)
-      when(ActiveMemoryBank === 1.U) { ROWBuffer := memory(1)(ActiveROWAddress_In_A_MemoryBank) }
-      when(ActiveMemoryBank === 2.U) { ROWBuffer := memory(2)(ActiveROWAddress_In_A_MemoryBank) }
-      when(ActiveMemoryBank === 3.U) { ROWBuffer := memory(3)(ActiveROWAddress_In_A_MemoryBank) }
-      state := state_idle
-    }
-    is(state_read) {
-      when(BurstCounter < (MR_CAS_Latency - 1.U)) {
-        BurstCounter := BurstCounter + 1.U
-      }.otherwise {
-        BurstCounter := 0.U
-        state := state_read_data
+      is(state_write_data) {
+        val col = ActiveColumnAddress_In_A_MemoryBank + BurstCounter
+        val old = ROWBuffer(col)
+        ROWBuffer(col) := Cat(
+          Mux(!io.dqm(1), input(15, 8), old(15, 8)),
+          Mux(!io.dqm(0), input(7, 0), old(7, 0))
+        )
+        when(MR_Write_Burst_Mode || BurstCounter === MR_Burst_Length - 1.U) {
+          state := state_idle // single write只要一拍
+        }.otherwise {
+          BurstCounter := BurstCounter + 1.U
+        }
       }
     }
-    is(state_read_data) {
-      en := true.B
-      output := ROWBuffer(ActiveColumnAddress_In_A_MemoryBank + BurstCounter)
-      when(BurstCounter === (MR_Burst_Length - 1.U)) {
-        state := state_idle
-      }.otherwise {
-        BurstCounter := BurstCounter + 1.U
-      }
-    }
-    is(state_write) {
-      state := state_write_data
-    }
-    is(state_write_data) {
-      val col = ActiveColumnAddress_In_A_MemoryBank + BurstCounter
-      val old = ROWBuffer(col)
-      ROWBuffer(col) := Cat(
-        Mux(!io.dqm(1), input(15, 8), old(15, 8)),
-        Mux(!io.dqm(0), input(7, 0),  old(7, 0))
-      )
-      when(MR_Write_Burst_Mode || BurstCounter === MR_Burst_Length - 1.U) {
-        state := state_idle // single write只要一拍
-      }.otherwise {
-        BurstCounter := BurstCounter + 1.U
-      }
   }
-  }
-}
 }
 
 class AXI4SDRAM(address: Seq[AddressSet])(implicit p: Parameters)
