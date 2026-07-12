973147c199681101db81553e5fe497660f75ee8f
tracer-ysyx <tracer@ysyx.org>
2026-07-12 22:30:21 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  22:30:21 up  1:58,  1 user,  load average: 0.81, 0.47, 0.36

--- ysyxSoC/src/SoC.scala
+++ ysyxSoC/src/SoC.scala
@@ -238,8 +238,23 @@ class ysyxSoCFull(implicit p: Parameters) extends LazyModule {
     // val psram = Module(new psram)
     val psram = Module(new psramChisel)
     psram.io <> masic.psram
-    val sdram = Module(new sdramChisel)
-    sdram.io <> masic.sdram
+    // 位扩展：实例化 2 个 16 位 SDRAM 颗粒，命令广播、dqm/dq 各接一半
+    val sdram0 = Module(new sdramChisel)
+    val sdram1 = Module(new sdramChisel)
+    Seq(sdram0, sdram1).foreach { s =>
+      s.io.clk := masic.sdram.clk
+      s.io.cke := masic.sdram.cke
+      s.io.cs  := masic.sdram.cs
+      s.io.ras := masic.sdram.ras
+      s.io.cas := masic.sdram.cas
+      s.io.we  := masic.sdram.we
+      s.io.a   := masic.sdram.a
+      s.io.ba  := masic.sdram.ba
+    }
+    sdram0.io.dqm := masic.sdram.dqm(1, 0)
+    sdram1.io.dqm := masic.sdram.dqm(3, 2)
+    chisel3.experimental.attach(sdram0.io.dq(0), masic.sdram.dq(0))
+    chisel3.experimental.attach(sdram1.io.dq(0), masic.sdram.dq(1))
 
     val externalPins = IO(new Bundle {
       val gpio = chiselTypeOf(masic.gpio)
