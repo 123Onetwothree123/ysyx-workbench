ce3844fc8dc987aea3e6b59470b6a99389a67b50
tracer-ysyx <tracer@ysyx.org>
2026-07-13 23:59:38 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  23:59:38 up 11:21,  1 user,  load average: 0.28, 0.50, 0.55

--- abstract-machine/am/src/riscv/ysyxsoc/ioe.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe.c
@@ -4,6 +4,7 @@ void __am_timer_init();
 void __am_timer_rtc(AM_TIMER_RTC_T *);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *);
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *);
+void __am_uart_rx(AM_UART_RX_T *);
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg)
 {
     cfg->present = true;
@@ -15,7 +16,7 @@ static void __am_input_config(AM_INPUT_CONFIG_T *cfg)
 }
 static void __am_uart_config(AM_UART_CONFIG_T *cfg)
 {
-    cfg->present = false;
+    cfg->present = true;
 }
 typedef void (*handler_t)(void *buf);
 bool ioe_init()
@@ -44,6 +45,9 @@ void ioe_read(int reg, void *buf)
         break;
     case AM_UART_CONFIG:
         __am_uart_config(buf);
+        break;
+    case AM_UART_RX:
+        __am_uart_rx(buf);
         break;
     default:
         panic("access nonexist register");
