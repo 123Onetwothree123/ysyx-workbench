2dcdb9d8f09d8df58d12634cccba112416ec81f3
tracer-ysyx <tracer@ysyx.org>
2026-07-13 13:22:07 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  13:22:07 up 44 min,  1 user,  load average: 0.37, 0.32, 0.29

--- npc/csrc/Test/NVBoardSegmentStudentID/NVBoardSegmentStudentID.cpp
+++ npc/csrc/Test/NVBoardSegmentStudentID/NVBoardSegmentStudentID.cpp
@@ -5,5 +5,13 @@ void nvboard_segment_student_id(std::uintptr_t SegmentRegister)
     auto *const SEG{reinterpret_cast<volatile std::uint32_t *>(SegmentRegister)};
     std::uint32_t MarchID{};
     asm volatile("csrr %0, 0xF12" : "=r"(MarchID));
-    *SEG = MarchID;
+
+    std::uint32_t Value{MarchID};
+    std::uint32_t BCD{0};
+    for (int i{0}; i < 8; ++i)
+    {
+        BCD |= (Value % 10U) << (4U * i);
+        Value /= 10U;
+    }
+    *SEG = BCD;
 }
