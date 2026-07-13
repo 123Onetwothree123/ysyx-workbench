e9343d95a6bc629d433f64a2cfe70b2046ff5691
tracer-ysyx <tracer@ysyx.org>
2026-07-13 22:36:28 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  22:36:28 up  9:58,  1 user,  load average: 0.58, 0.82, 0.97

--- npc/csrc/Test/NVBoardUARTTest/main.cpp
+++ npc/csrc/Test/NVBoardUARTTest/main.cpp
@@ -1,15 +1,14 @@
 #include <am.h>
+#include <klib-macros.h>
 
 int main(const char *)
 {
-    volatile unsigned char *LSR = (volatile unsigned char *)0x10000005;
-    volatile unsigned int *RBR = (volatile unsigned int *)0x10000000;
-    for (const char *p{"UART RX Test\n"}; *p; ++p) putch(*p);
+    for (const char *p{"UART NonBlock Test\n"}; *p; ++p) putch(*p);
     while (1)
     {
-        if (*LSR & 0x01)
+        char Ch = io_read(AM_UART_RX).data;
+        if (Ch != (char)-1)
         {
-            char Ch = (char)(*RBR & 0xff);
             putch('<');
             putch(Ch);
             putch('>');
