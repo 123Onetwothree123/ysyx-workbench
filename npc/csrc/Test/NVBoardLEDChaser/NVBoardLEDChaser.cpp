e61389a76e112e5f2a44106fc2bb60f5aee56027
tracer-ysyx <tracer@ysyx.org>
2026-07-13 16:46:41 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  16:46:41 up  4:09,  1 user,  load average: 0.59, 0.46, 0.47

--- npc/csrc/Test/NVBoardLEDChaser/NVBoardLEDChaser.cpp
+++ npc/csrc/Test/NVBoardLEDChaser/NVBoardLEDChaser.cpp
@@ -9,10 +9,7 @@ static void DelayMs(std::uint32_t Ms)
     }
 }
 
-extern "C" void nvboard_led_chaser(std::uintptr_t LEDRegister,
-                                   std::size_t LEDCount,
-                                   std::uint32_t StepDelayMs,
-                                   std::size_t rounds)
+extern "C" void nvboard_led_chaser(std::uintptr_t LEDRegister, std::size_t LEDCount, std::uint32_t StepDelayMs, std::size_t rounds)
 {
     auto *const LED{reinterpret_cast<volatile std::uint32_t *>(LEDRegister)};
     for (std::size_t Round{0}; rounds == 0 || Round < rounds; ++Round)
