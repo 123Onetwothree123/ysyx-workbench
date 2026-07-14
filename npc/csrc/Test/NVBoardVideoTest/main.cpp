22824f7bdbb4aff13cc5ff3b3b6d85382115fe8e
tracer-ysyx <tracer@ysyx.org>
2026-07-14 18:00:59 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  18:00:59 up  9:19,  1 user,  load average: 0.75, 0.79, 0.97

--- npc/csrc/Test/NVBoardVideoTest/main.cpp
+++ npc/csrc/Test/NVBoardVideoTest/main.cpp
@@ -5,12 +5,11 @@ int main(const char *)
 {
     for (const char *p{"GPU Test\n"}; *p; ++p) putch(*p);
 
-    unsigned int Row[80];
-    for (int Y = 0; Y < 480; ++Y)
+    unsigned int Row[40];
+    for (int Y = 0; Y < 10; ++Y)
     {
-        int Band = Y / 60;
         unsigned int C;
-        switch (Band)
+        switch (Y)
         {
         case 0: C = 0x00FF0000U; break;
         case 1: C = 0x00FF7F00U; break;
@@ -19,10 +18,12 @@ int main(const char *)
         case 4: C = 0x000000FFU; break;
         case 5: C = 0x004B0082U; break;
         case 6: C = 0x00FF00FFU; break;
-        default: C = 0x00FFFFFFU; break;
+        case 7: C = 0x0000FFFFU; break;
+        case 8: C = 0x00FFFFFFU; break;
+        default: C = 0x00808080U; break;
         }
-        for (int X = 0; X < 80; ++X) Row[X] = C;
-        io_write(AM_GPU_FBDRAW, 0, Y, Row, 80, 1, false);
+        for (int X = 0; X < 40; ++X) Row[X] = C;
+        io_write(AM_GPU_FBDRAW, 100, 200 + Y, Row, 40, 1, false);
     }
 
     for (const char *p{"GPU Done\n"}; *p; ++p) putch(*p);
