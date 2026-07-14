fb9e9f6e641426bd5a25201ce95f777d5f82f3df
tracer-ysyx <tracer@ysyx.org>
2026-07-14 18:11:37 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  18:11:37 up  9:30,  1 user,  load average: 0.24, 0.76, 0.92

--- npc/csrc/Test/NVBoardVideoTest/main.cpp
+++ npc/csrc/Test/NVBoardVideoTest/main.cpp
@@ -1,29 +1,15 @@
 #include <am.h>
-#include <klib-macros.h>
 
 int main(const char *)
 {
-    for (const char *p{"FB R/W Test\n"}; *p; ++p) putch(*p);
+    for (const char *p{"Fill\n"}; *p; ++p) putch(*p);
 
     volatile unsigned int *FB = (volatile unsigned int *)0x21000000U;
+    for (int y = 0; y < 480; ++y)
+        for (int x = 0; x < 640; ++x)
+            FB[y * 640 + x] = 0x00FF0000U;
 
-    FB[100] = 0x00FF0000U;
-    FB[200] = 0x0000FF00U;
-    FB[300] = 0x000000FFU;
-
-    unsigned int A = FB[100];
-    unsigned int B = FB[200];
-    unsigned int C = FB[300];
-
-    if (A == 0x00FF0000U && B == 0x0000FF00U && C == 0x000000FFU)
-    {
-        for (const char *p{"FB OK\n"}; *p; ++p) putch(*p);
-    }
-    else
-    {
-        for (const char *p{"FB FAIL\n"}; *p; ++p) putch(*p);
-    }
-
+    for (const char *p{"Done\n"}; *p; ++p) putch(*p);
     while (1) {}
     return 0;
 }
