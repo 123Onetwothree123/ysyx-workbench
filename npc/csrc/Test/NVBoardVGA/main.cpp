511854ccb331685b9914a47c9add2541024973a0
tracer-ysyx <tracer@ysyx.org>
2026-07-14 11:53:30 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  11:53:30 up  3:12,  1 user,  load average: 2.07, 2.07, 2.05

--- npc/csrc/Test/NVBoardVGA/main.cpp
+++ npc/csrc/Test/NVBoardVGA/main.cpp
@@ -1,10 +1,29 @@
 #include <am.h>
-int main(const char *) {
+int main(const char *)
+{
     volatile unsigned int *FB = (volatile unsigned int *)0x21000000U;
+
+    for (int y = 0; y < 480; ++y)
+    {
+        unsigned int Color;
+        if (y < 100)
+            Color = 0x00FF0000U;
+        else if (y < 200)
+            Color = 0x0000FF00U;
+        else if (y < 300)
+            Color = 0x000000FFU;
+        else if (y < 400)
+            Color = 0x00FFFF00U;
+        else
+            Color = 0x00FF00FFU;
+
+        FB[y * 640 + 10] = Color;
+        FB[y * 640 + 629] = Color;
+    }
+
     for (int x = 0; x < 640; ++x)
-        FB[x] = 0x0000FF00U;
-    for (int x = 0; x < 640; ++x)
-        FB[100 * 640 + x] = 0x00FF0000U;
+        FB[239 * 640 + x] = 0x00FFFFFFU;
+
     while (1) {}
     return 0;
 }
