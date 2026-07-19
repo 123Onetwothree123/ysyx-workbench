bccee82e3af02ef60a18f9cd031f9c5f5f766d9c
tracer-ysyx <tracer@ysyx.org>
2026-07-19 15:44:29 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  15:44:29 up  6:05,  1 user,  load average: 2.86, 1.28, 0.84

--- npc/src/ysyx_26030103_Elaborate.scala
+++ npc/src/ysyx_26030103_Elaborate.scala
@@ -6,29 +6,30 @@ object ysyx_26030103_Elaborate extends App {
   emitVerilog(new ysyx_26030103, Array("--target-dir", targetDir))
   emitVerilog(new _root_.ysyx_26030103.riscv32e_npc_SimTop, Array("--target-dir", targetDir))
 
-  // firtool会在顶层.sv里生成一些`include "layers-*.sv"`，但emitVerilog有时不会把这些文件拆出来。
-  // Verilator看到include却找不到文件就会报错，所以这里给缺失的layers文件补空壳。
-  val rv32iSV = scala.io.Source.fromFile(targetDir + "/ysyx_26030103.sv")
-  val includePattern = """`include "([^"]*layers-[^"]*\.sv)"""".r
-  val layerFiles =
-    try {
-      rv32iSV
-        .getLines()
-        .flatMap(line => includePattern.findAllMatchIn(line).map(_.group(1)))
-        .toSet
-    } finally {
-      rv32iSV.close()
-    }
+  for (top <- Seq("ysyx_26030103", "riscv32e_npc_SimTop")) {
+    val svFile = s"$targetDir/$top.sv"
+    val includePattern = """`include "([^"]*layers-[^"]*\.sv)"""".r
+    val rvSV = scala.io.Source.fromFile(svFile)
+    val layerFiles =
+      try {
+        rvSV
+          .getLines()
+          .flatMap(line => includePattern.findAllMatchIn(line).map(_.group(1)))
+          .toSet
+      } finally {
+        rvSV.close()
+      }
 
-  for (file <- layerFiles) {
-    val out = new java.io.File(targetDir + "/" + file)
-    if (!out.exists()) {
-      val name = file.stripSuffix(".sv").replace("-", "_")
-      val pw = new java.io.PrintWriter(out)
-      pw.println(s"`ifndef $name")
-      pw.println(s"  `define $name")
-      pw.println("`endif")
-      pw.close()
+    for (file <- layerFiles) {
+      val out = new java.io.File(targetDir + "/" + file)
+      if (!out.exists()) {
+        val name = file.stripSuffix(".sv").replace("-", "_")
+        val pw = new java.io.PrintWriter(out)
+        pw.println(s"`ifndef $name")
+        pw.println(s"  `define $name")
+        pw.println("`endif")
+        pw.close()
+      }
     }
   }
 }
