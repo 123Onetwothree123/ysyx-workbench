b07e1c8556634e08997397999e9520d3cd04865c
tracer-ysyx <tracer@ysyx.org>
2026-07-19 15:52:32 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  15:52:32 up  6:13,  1 user,  load average: 1.84, 1.91, 1.35

--- npc/src/riscv32e_npc.scala
+++ npc/src/riscv32e_npc.scala
@@ -73,135 +73,8 @@ class riscv32e_npc_SimTop extends Module {
   val cpu = Module(new ysyx_26030103)
   val ram = Module(new riscv32e_npc_AXIRAM)
 
-  // 逐端口声明 IO，与 ysyxSoCFull 命名一致（无 io_ 前缀）
-  val clockIO = IO(Input(Clock()))
-  val resetIO = IO(Input(Reset()))
-  val interrupt = IO(Input(Bool()))
-  val trap_valid = IO(Output(Bool()))
-  val trap_pc = IO(Output(UInt(32.W)))
+  cpu.io.interrupt := false.B
 
-  val master_awready = IO(Input(Bool()))
-  val master_awvalid = IO(Output(Bool()))
-  val master_awaddr  = IO(Output(UInt(32.W)))
-  val master_awid    = IO(Output(UInt(4.W)))
-  val master_awlen   = IO(Output(UInt(8.W)))
-  val master_awsize  = IO(Output(UInt(3.W)))
-  val master_awburst = IO(Output(UInt(2.W)))
-  val master_awprot  = IO(Output(UInt(3.W)))
-
-  val master_wready = IO(Input(Bool()))
-  val master_wvalid = IO(Output(Bool()))
-  val master_wdata  = IO(Output(UInt(32.W)))
-  val master_wstrb  = IO(Output(UInt(4.W)))
-  val master_wlast  = IO(Output(Bool()))
-
-  val master_bready = IO(Output(Bool()))
-  val master_bvalid = IO(Input(Bool()))
-  val master_bresp  = IO(Input(UInt(2.W)))
-  val master_bid    = IO(Input(UInt(4.W)))
-
-  val master_arready = IO(Input(Bool()))
-  val master_arvalid = IO(Output(Bool()))
-  val master_araddr  = IO(Output(UInt(32.W)))
-  val master_arid    = IO(Output(UInt(4.W)))
-  val master_arlen   = IO(Output(UInt(8.W)))
-  val master_arsize  = IO(Output(UInt(3.W)))
-  val master_arburst = IO(Output(UInt(2.W)))
-  val master_arprot  = IO(Output(UInt(3.W)))
-
-  val master_rready = IO(Output(Bool()))
-  val master_rvalid = IO(Input(Bool()))
-  val master_rresp  = IO(Input(UInt(2.W)))
-  val master_rdata  = IO(Input(UInt(32.W)))
-  val master_rlast  = IO(Input(Bool()))
-  val master_rid    = IO(Input(UInt(4.W)))
-
-  val slave_awready = IO(Output(Bool()))
-  val slave_awvalid = IO(Input(Bool()))
-  val slave_awaddr  = IO(Input(UInt(32.W)))
-  val slave_awid    = IO(Input(UInt(4.W)))
-  val slave_awlen   = IO(Input(UInt(8.W)))
-  val slave_awsize  = IO(Input(UInt(3.W)))
-  val slave_awburst = IO(Input(UInt(2.W)))
-  val slave_awlock  = IO(Input(Bool()))
-  val slave_awcache = IO(Input(UInt(4.W)))
-  val slave_awprot  = IO(Input(UInt(3.W)))
-  val slave_awqos   = IO(Input(UInt(4.W)))
-
-  val slave_wready = IO(Output(Bool()))
-  val slave_wvalid = IO(Input(Bool()))
-  val slave_wdata  = IO(Input(UInt(32.W)))
-  val slave_wstrb  = IO(Input(UInt(4.W)))
-  val slave_wlast  = IO(Input(Bool()))
-
-  val slave_bready = IO(Input(Bool()))
-  val slave_bvalid = IO(Output(Bool()))
-  val slave_bresp  = IO(Output(UInt(2.W)))
-  val slave_bid    = IO(Output(UInt(4.W)))
-
-  val slave_arready = IO(Output(Bool()))
-  val slave_arvalid = IO(Input(Bool()))
-  val slave_araddr  = IO(Input(UInt(32.W)))
-  val slave_arid    = IO(Input(UInt(4.W)))
-  val slave_arlen   = IO(Input(UInt(8.W)))
-  val slave_arsize  = IO(Input(UInt(3.W)))
-  val slave_arburst = IO(Input(UInt(2.W)))
-  val slave_arlock  = IO(Input(Bool()))
-  val slave_arcache = IO(Input(UInt(4.W)))
-  val slave_arprot  = IO(Input(UInt(3.W)))
-  val slave_arqos   = IO(Input(UInt(4.W)))
-
-  val slave_rready = IO(Input(Bool()))
-  val slave_rvalid = IO(Output(Bool()))
-  val slave_rresp  = IO(Output(UInt(2.W)))
-  val slave_rdata  = IO(Output(UInt(32.W)))
-  val slave_rlast  = IO(Output(Bool()))
-  val slave_rid    = IO(Output(UInt(4.W)))
-
-  val debug_gpr_raddr = IO(Input(UInt(5.W)))
-  val debug_gpr_rdata = IO(Output(UInt(32.W)))
-  val debug_pc = IO(Output(UInt(32.W)))
-  val debug_instructions = IO(Output(UInt(32.W)))
-  val debug_mtrace_valid = IO(Output(Bool()))
-  val debug_mtrace_wen   = IO(Output(Bool()))
-  val debug_mtrace_addr  = IO(Output(UInt(32.W)))
-  val debug_mtrace_wdata = IO(Output(UInt(32.W)))
-  val debug_mtrace_rdata = IO(Output(UInt(32.W)))
-  val debug_mtrace_width = IO(Output(UInt(2.W)))
-  val debug_access_fault = IO(Output(Bool()))
-  val debug_access_fault_resp = IO(Output(UInt(2.W)))
-  val debug_commit = IO(Output(Bool()))
-
-  val perf_ifu_fetch  = IO(Output(Bool()))
-  val perf_exu_done   = IO(Output(Bool()))
-  val perf_lsu_load   = IO(Output(Bool()))
-  val perf_lsu_store  = IO(Output(Bool()))
-  val perf_alu_op     = IO(Output(Bool()))
-  val perf_mem_op     = IO(Output(Bool()))
-  val perf_csr_op     = IO(Output(Bool()))
-  val perf_branch_op  = IO(Output(Bool()))
-  val perf_ifu_stall_pipeline = IO(Output(Bool()))
-  val perf_ifu_stall_axi      = IO(Output(Bool()))
-  val perf_ifu_stall_ar       = IO(Output(Bool()))
-  val perf_ifu_stall_r        = IO(Output(Bool()))
-  val perf_ifu_stall_redirect = IO(Output(Bool()))
-  val perf_ifu_stall_idle     = IO(Output(Bool()))
-  val perf_execution_active   = IO(Output(Bool()))
-  val perf_exu_stall_lsu      = IO(Output(Bool()))
-  val perf_lsu_active         = IO(Output(Bool()))
-  val perf_lsu_load_active    = IO(Output(Bool()))
-  val perf_lsu_store_active   = IO(Output(Bool()))
-  val perf_lsu_stall_read_ar  = IO(Output(Bool()))
-  val perf_lsu_stall_read_r   = IO(Output(Bool()))
-  val perf_lsu_stall_write_req = IO(Output(Bool()))
-  val perf_lsu_stall_write_b  = IO(Output(Bool()))
-
-  // 连接时钟复位
-  cpu.clock := clockIO
-  cpu.reset := resetIO
-  cpu.io.interrupt := interrupt
-
-  // AXI master → RAM
   cpu.io.master_awready := ram.io.axi.AW.AWREADY
   ram.io.axi.AW.AWVALID := cpu.io.master_awvalid
   ram.io.axi.AW.AWADDR  := cpu.io.master_awaddr
@@ -238,93 +111,91 @@ class riscv32e_npc_SimTop extends Module {
   cpu.io.master_rid    := ram.io.axi.R.RID
   ram.io.axi.R.RREADY  := cpu.io.master_rready
 
-  // slave 悬空
   cpu.io.slave_awvalid := false.B
   cpu.io.slave_wvalid  := false.B
   cpu.io.slave_bready  := false.B
   cpu.io.slave_arvalid := false.B
   cpu.io.slave_rready  := false.B
 
-  // 端口透传
-  interrupt           := false.B
-  trap_valid          := cpu.io.trap_valid
-  trap_pc             := cpu.io.trap_pc
-  master_awready      := DontCare
-  master_awvalid      := DontCare
-  master_awaddr       := DontCare
-  master_awid         := DontCare
-  master_awlen        := DontCare
-  master_awsize       := DontCare
-  master_awburst      := DontCare
-  master_awprot       := DontCare
-  master_wready       := DontCare
-  master_wvalid       := DontCare
-  master_wdata        := DontCare
-  master_wstrb        := DontCare
-  master_wlast        := DontCare
-  master_bready       := DontCare
-  master_bvalid       := DontCare
-  master_bresp        := DontCare
-  master_bid          := DontCare
-  master_arready      := DontCare
-  master_arvalid      := DontCare
-  master_araddr       := DontCare
-  master_arid         := DontCare
-  master_arlen        := DontCare
-  master_arsize       := DontCare
-  master_arburst      := DontCare
-  master_arprot       := DontCare
-  master_rready       := DontCare
-  master_rvalid       := DontCare
-  master_rresp        := DontCare
-  master_rdata        := DontCare
-  master_rlast        := DontCare
-  master_rid          := DontCare
-  slave_awready       := DontCare
-  slave_wready        := DontCare
-  slave_bvalid        := DontCare
-  slave_bresp         := DontCare
-  slave_bid           := DontCare
-  slave_arready       := DontCare
-  slave_rvalid        := DontCare
-  slave_rresp         := DontCare
-  slave_rdata         := DontCare
-  slave_rlast         := DontCare
-  slave_rid           := DontCare
-  debug_gpr_raddr     := cpu.io.debug_gpr_raddr
-  debug_gpr_rdata     := cpu.io.debug_gpr_rdata
-  debug_pc            := cpu.io.debug_pc
-  debug_instructions  := cpu.io.debug_instructions
-  debug_mtrace_valid  := cpu.io.debug_mtrace_valid
-  debug_mtrace_wen    := cpu.io.debug_mtrace_wen
-  debug_mtrace_addr   := cpu.io.debug_mtrace_addr
-  debug_mtrace_wdata  := cpu.io.debug_mtrace_wdata
-  debug_mtrace_rdata  := cpu.io.debug_mtrace_rdata
-  debug_mtrace_width  := cpu.io.debug_mtrace_width
-  debug_access_fault  := cpu.io.debug_access_fault
+  val trap_valid = IO(Output(Bool()))
+  trap_valid := cpu.io.trap_valid
+  val trap_pc = IO(Output(UInt(32.W)))
+  trap_pc := cpu.io.trap_pc
+
+  val debug_gpr_raddr = IO(Input(UInt(5.W)))
+  cpu.io.debug_gpr_raddr := debug_gpr_raddr
+  val debug_gpr_rdata = IO(Output(UInt(32.W)))
+  debug_gpr_rdata := cpu.io.debug_gpr_rdata
+  val debug_pc = IO(Output(UInt(32.W)))
+  debug_pc := cpu.io.debug_pc
+  val debug_instructions = IO(Output(UInt(32.W)))
+  debug_instructions := cpu.io.debug_instructions
+
+  val debug_mtrace_valid = IO(Output(Bool()))
+  debug_mtrace_valid := cpu.io.debug_mtrace_valid
+  val debug_mtrace_wen = IO(Output(Bool()))
+  debug_mtrace_wen := cpu.io.debug_mtrace_wen
+  val debug_mtrace_addr = IO(Output(UInt(32.W)))
+  debug_mtrace_addr := cpu.io.debug_mtrace_addr
+  val debug_mtrace_wdata = IO(Output(UInt(32.W)))
+  debug_mtrace_wdata := cpu.io.debug_mtrace_wdata
+  val debug_mtrace_rdata = IO(Output(UInt(32.W)))
+  debug_mtrace_rdata := cpu.io.debug_mtrace_rdata
+  val debug_mtrace_width = IO(Output(UInt(2.W)))
+  debug_mtrace_width := cpu.io.debug_mtrace_width
+
+  val debug_access_fault = IO(Output(Bool()))
+  debug_access_fault := cpu.io.debug_access_fault
+  val debug_access_fault_resp = IO(Output(UInt(2.W)))
   debug_access_fault_resp := cpu.io.debug_access_fault_resp
-  debug_commit        := cpu.io.debug_commit
-  perf_ifu_fetch      := cpu.io.perf_ifu_fetch
-  perf_exu_done       := cpu.io.perf_exu_done
-  perf_lsu_load       := cpu.io.perf_lsu_load
-  perf_lsu_store      := cpu.io.perf_lsu_store
-  perf_alu_op         := cpu.io.perf_alu_op
-  perf_mem_op         := cpu.io.perf_mem_op
-  perf_csr_op         := cpu.io.perf_csr_op
-  perf_branch_op      := cpu.io.perf_branch_op
+  val debug_commit = IO(Output(Bool()))
+  debug_commit := cpu.io.debug_commit
+
+  val perf_ifu_fetch = IO(Output(Bool()))
+  perf_ifu_fetch := cpu.io.perf_ifu_fetch
+  val perf_exu_done = IO(Output(Bool()))
+  perf_exu_done := cpu.io.perf_exu_done
+  val perf_lsu_load = IO(Output(Bool()))
+  perf_lsu_load := cpu.io.perf_lsu_load
+  val perf_lsu_store = IO(Output(Bool()))
+  perf_lsu_store := cpu.io.perf_lsu_store
+  val perf_alu_op = IO(Output(Bool()))
+  perf_alu_op := cpu.io.perf_alu_op
+  val perf_mem_op = IO(Output(Bool()))
+  perf_mem_op := cpu.io.perf_mem_op
+  val perf_csr_op = IO(Output(Bool()))
+  perf_csr_op := cpu.io.perf_csr_op
+  val perf_branch_op = IO(Output(Bool()))
+  perf_branch_op := cpu.io.perf_branch_op
+
+  val perf_ifu_stall_pipeline = IO(Output(Bool()))
   perf_ifu_stall_pipeline := cpu.io.perf_ifu_stall_pipeline
-  perf_ifu_stall_axi      := cpu.io.perf_ifu_stall_axi
-  perf_ifu_stall_ar       := cpu.io.perf_ifu_stall_ar
-  perf_ifu_stall_r        := cpu.io.perf_ifu_stall_r
+  val perf_ifu_stall_axi = IO(Output(Bool()))
+  perf_ifu_stall_axi := cpu.io.perf_ifu_stall_axi
+  val perf_ifu_stall_ar = IO(Output(Bool()))
+  perf_ifu_stall_ar := cpu.io.perf_ifu_stall_ar
+  val perf_ifu_stall_r = IO(Output(Bool()))
+  perf_ifu_stall_r := cpu.io.perf_ifu_stall_r
+  val perf_ifu_stall_redirect = IO(Output(Bool()))
   perf_ifu_stall_redirect := cpu.io.perf_ifu_stall_redirect
-  perf_ifu_stall_idle     := cpu.io.perf_ifu_stall_idle
-  perf_execution_active   := cpu.io.perf_execution_active
-  perf_exu_stall_lsu      := cpu.io.perf_exu_stall_lsu
-  perf_lsu_active         := cpu.io.perf_lsu_active
-  perf_lsu_load_active    := cpu.io.perf_lsu_load_active
-  perf_lsu_store_active   := cpu.io.perf_lsu_store_active
-  perf_lsu_stall_read_ar  := cpu.io.perf_lsu_stall_read_ar
-  perf_lsu_stall_read_r   := cpu.io.perf_lsu_stall_read_r
+  val perf_ifu_stall_idle = IO(Output(Bool()))
+  perf_ifu_stall_idle := cpu.io.perf_ifu_stall_idle
+  val perf_execution_active = IO(Output(Bool()))
+  perf_execution_active := cpu.io.perf_execution_active
+  val perf_exu_stall_lsu = IO(Output(Bool()))
+  perf_exu_stall_lsu := cpu.io.perf_exu_stall_lsu
+  val perf_lsu_active = IO(Output(Bool()))
+  perf_lsu_active := cpu.io.perf_lsu_active
+  val perf_lsu_load_active = IO(Output(Bool()))
+  perf_lsu_load_active := cpu.io.perf_lsu_load_active
+  val perf_lsu_store_active = IO(Output(Bool()))
+  perf_lsu_store_active := cpu.io.perf_lsu_store_active
+  val perf_lsu_stall_read_ar = IO(Output(Bool()))
+  perf_lsu_stall_read_ar := cpu.io.perf_lsu_stall_read_ar
+  val perf_lsu_stall_read_r = IO(Output(Bool()))
+  perf_lsu_stall_read_r := cpu.io.perf_lsu_stall_read_r
+  val perf_lsu_stall_write_req = IO(Output(Bool()))
   perf_lsu_stall_write_req := cpu.io.perf_lsu_stall_write_req
-  perf_lsu_stall_write_b  := cpu.io.perf_lsu_stall_write_b
+  val perf_lsu_stall_write_b = IO(Output(Bool()))
+  perf_lsu_stall_write_b := cpu.io.perf_lsu_stall_write_b
 }
