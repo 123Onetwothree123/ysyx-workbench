b8db420bf350e277c0c6a00873df3066b57a6478
100017119-Lin Junjie <alsttabblt@outlook.com>
2026-07-19 11:10:57 +0800

    实现了延迟模块功能，并且检查新的瓶颈，AXI这次时间占比达到了97.7%，IPC下降到了ipc = 0.0081 MicroBench PASS Scored time: 33617.976 ms Total  time: 47650.800 ms trap了 HIT GOOD TRAP at pc = 0xa0004dc8, cycles = 49858886, instructions = 406261, ipc = 0.0081 性能计数器 IFU取到指令: 574458 EXU完成计算: 574458 LSU取到数据: 67434 LSU写出数据: 54383 ALU指令: 305759 访存指令: 121817 CSR指令: 0 分支/跳转指令: 146882 指令类别合计: 574458 (应与IFU取指一致) IFU取指: 574458 EXU完成: 574458 (应与IFU取指接近) LSU合计: 121817 (应与访存指令一致)
    指令类别占比与平均周期:
    ALU指令占比: 53.2%
    访存指令占比: 21.2%
    访存指令平均周期: 77.88
    CSR指令占比: 0.0%
    分支/跳转指令占比: 25.6%
    
    IFU取不到指令原因分析:
    流水线阻塞(IFU有数据但下游不接): 574458 周期, 占比 1.2%
    AXI总线等待(AR/R通道未就绪): 48709966 周期, 占比 97.7%
    跳转冲刷(取指结果被丢弃): 93126 周期, 占比 0.2%
    
    LSU平均访存延迟:
    平均延迟: 76.88 周期

--- ysyxSoC/src/amba/APBDelayer.scala
+++ ysyxSoC/src/amba/APBDelayer.scala
@@ -12,7 +12,9 @@ import freechips.rocketchip.util._
 class APBDelayerIO extends Bundle {
   val clock = Input(Clock())
   val reset = Input(Reset())
-  val in = Flipped(new APBBundle(APBBundleParameters(addrBits = 32, dataBits = 32)))
+  val in = Flipped(
+    new APBBundle(APBBundleParameters(addrBits = 32, dataBits = 32))
+  )
   val out = new APBBundle(APBBundleParameters(addrBits = 32, dataBits = 32))
 }
 
@@ -23,6 +25,76 @@ class apb_delayer extends BlackBox {
 class APBDelayerChisel extends Module {
   val io = IO(new APBDelayerIO)
   io.out <> io.in
+  val CPU_MHZ = 1200
+  val DEVICE_MHZ = 100
+  val S = 64
+  val RTS = CPU_MHZ * S / DEVICE_MHZ // 因为r * S，整数截断
+  val AMT = if (RTS > S) {
+    RTS - S // (r-1) * S 的整数近似
+  } else {
+    0
+  }
+  if (AMT == 0) {
+    // r <= 1, 无需延迟, 直通
+    io.out <> io.in
+  } else {
+    val CounterWidth = 24
+    val state_idle :: state_count :: state_delay :: Nil = Enum(3)
+    val state = RegInit(state_idle)
+    val counter = RegInit(0.S(CounterWidth.W))
+    val rdata = RegInit(0.U(32.W))
+    val slverr = RegInit(false.B)
+    io.out.psel := io.in.psel
+    io.out.penable := io.in.penable
+    io.out.pwrite := io.in.pwrite
+    io.out.paddr := io.in.paddr
+    io.out.pprot := io.in.pprot
+    io.out.pwdata := io.in.pwdata
+    io.out.pstrb := io.in.pstrb
+    io.out.pauser := io.in.pauser
+    io.in.pready := false.B
+    io.in.prdata := 0.U
+    io.in.pslverr := false.B
+    io.in.pduser := io.out.pduser
+    switch(state) {
+      is(state_idle) {
+        // 空闲时直通，用于检测事务开始
+        io.in.pready := io.out.pready
+        io.in.prdata := io.out.prdata
+        io.in.pslverr := io.out.pslverr
+        when(io.in.psel && !io.in.penable) {
+          state := state_count
+          counter := AMT.S(CounterWidth.W)
+        }
+      }
+      is(state_count) {
+        when(!io.in.psel) {
+          // 事务异常中止
+          state := state_idle
+        }.elsewhen(io.out.pready && io.in.penable) {
+          // 设备回复到达之后就捕获数据，进入排空阶段
+          state := state_delay
+          rdata := io.out.prdata
+          slverr := io.out.pslverr
+          // 本周期不再累加（设备已完成处理）
+        }.otherwise {
+          // 等待设备处理，每周期累加(r-1)*S
+          counter := counter + AMT.S(CounterWidth.W)
+        }
+      }
+      is(state_delay) {
+        io.in.prdata := rdata
+        io.in.pslverr := slverr
+        when(counter <= 0.S) {
+          // 排空完毕，向上游返回pready
+          state := state_idle
+          io.in.pready := true.B
+        }.otherwise {
+          counter := counter - S.S
+        }
+      }
+    }
+  }
 }
 
 class APBDelayerWrapper(implicit p: Parameters) extends LazyModule {
@@ -31,7 +103,8 @@ class APBDelayerWrapper(implicit p: Parameters) extends LazyModule {
   lazy val module = new Impl
   class Impl extends LazyModuleImp(this) {
     (node.in zip node.out) foreach { case ((in, edgeIn), (out, edgeOut)) =>
-      val delayer = Module(new apb_delayer)
+      // val delayer = Module(new apb_delayer)
+      val delayer = Module(new APBDelayerChisel)
       delayer.io.clock := clock
       delayer.io.reset := reset
       delayer.io.in <> in
