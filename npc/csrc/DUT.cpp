4a0c02cefb28daf95ed8e8bb562958aa6f5729c8
tracer-ysyx <tracer@ysyx.org>
2026-07-19 13:05:09 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  13:05:09 up  3:26,  1 user,  load average: 0.17, 0.22, 0.33

--- npc/csrc/DUT.cpp
+++ npc/csrc/DUT.cpp
@@ -128,36 +128,117 @@ void DUT::step()
     {
         ++instructions;
     }
-    if (dut->perf_ifu_fetch)  ++instruction_fetch_count;
-    if (dut->perf_exu_done)   ++execution_complete_count;
-    if (dut->perf_lsu_load)   ++load_data_count;
-    if (dut->perf_lsu_store)  ++store_data_count;
+    if (dut->perf_ifu_fetch)
+    {
+        ++instruction_fetch_count;
+    }
     if (dut->perf_exu_done)
     {
-        if (dut->perf_alu_op)     ++arithmetic_operation_count;
-        if (dut->perf_mem_op)     ++memory_access_operation_count;
-        if (dut->perf_csr_op)     ++control_status_register_operation_count;
-        if (dut->perf_branch_op)  ++branch_operation_count;
+        ++execution_complete_count;
     }
-    if (dut->perf_ifu_stall_pipeline) ++instruction_fetch_stall_pipeline_count;
-    if (dut->perf_ifu_stall_axi)      ++instruction_fetch_stall_axi_count;
-    if (dut->perf_ifu_stall_ar)       ++instruction_fetch_stall_ar_count;
-    if (dut->perf_ifu_stall_r)        ++instruction_fetch_stall_r_count;
-    if (dut->perf_ifu_stall_redirect) ++instruction_fetch_stall_redirect_count;
-    if (dut->perf_ifu_stall_idle)     ++instruction_fetch_stall_idle_count;
-    if (dut->perf_execution_active)   ++execution_active_cycle_count;
-    if (dut->perf_exu_stall_lsu)      ++exu_stall_lsu_count;
-    if (dut->perf_execution_active && dut->perf_alu_op)    ++arithmetic_operation_active_cycle_count;
-    if (dut->perf_execution_active && dut->perf_mem_op)    ++memory_access_operation_active_cycle_count;
-    if (dut->perf_execution_active && dut->perf_csr_op)    ++control_status_register_operation_active_cycle_count;
-    if (dut->perf_execution_active && dut->perf_branch_op) ++branch_operation_active_cycle_count;
-    if (dut->perf_lsu_active)         ++load_store_unit_active_cycle_count;
-    if (dut->perf_lsu_load_active)    ++load_store_unit_load_active_cycle_count;
-    if (dut->perf_lsu_store_active)   ++load_store_unit_store_active_cycle_count;
-    if (dut->perf_lsu_stall_read_ar)  ++lsu_stall_read_ar_count;
-    if (dut->perf_lsu_stall_read_r)   ++lsu_stall_read_r_count;
-    if (dut->perf_lsu_stall_write_req) ++lsu_stall_write_req_count;
-    if (dut->perf_lsu_stall_write_b)  ++lsu_stall_write_b_count;
+    if (dut->perf_lsu_load)
+    {
+        ++load_data_count;
+    }
+    if (dut->perf_lsu_store)
+    {
+        ++store_data_count;
+    }
+    if (dut->perf_exu_done)
+    {
+        if (dut->perf_alu_op)
+        {
+            ++arithmetic_operation_count;
+        }
+        if (dut->perf_mem_op)
+        {
+            ++memory_access_operation_count;
+        }
+        if (dut->perf_csr_op)
+        {
+            ++control_status_register_operation_count;
+        }
+        if (dut->perf_branch_op)
+        {
+            ++branch_operation_count;
+        }
+    }
+    if (dut->perf_ifu_stall_pipeline)
+    {
+        ++instruction_fetch_stall_pipeline_count;
+    }
+    if (dut->perf_ifu_stall_axi)
+    {
+        ++instruction_fetch_stall_axi_count;
+    }
+    if (dut->perf_ifu_stall_ar)
+    {
+        ++instruction_fetch_stall_ar_count;
+    }
+    if (dut->perf_ifu_stall_r)
+    {
+        ++instruction_fetch_stall_r_count;
+    }
+    if (dut->perf_ifu_stall_redirect)
+    {
+        ++instruction_fetch_stall_redirect_count;
+    }
+    if (dut->perf_ifu_stall_idle)
+    {
+        ++instruction_fetch_stall_idle_count;
+    }
+    if (dut->perf_execution_active)
+    {
+        ++execution_active_cycle_count;
+    }
+    if (dut->perf_exu_stall_lsu)
+    {
+        ++exu_stall_lsu_count;
+    }
+    if (dut->perf_execution_active && dut->perf_alu_op)
+    {
+        ++arithmetic_operation_active_cycle_count;
+    }
+    if (dut->perf_execution_active && dut->perf_mem_op)
+    {
+        ++memory_access_operation_active_cycle_count;
+    }
+    if (dut->perf_execution_active && dut->perf_csr_op)
+    {
+        ++control_status_register_operation_active_cycle_count;
+    }
+    if (dut->perf_execution_active && dut->perf_branch_op)
+    {
+        ++branch_operation_active_cycle_count;
+    }
+    if (dut->perf_lsu_active)
+    {
+        ++load_store_unit_active_cycle_count;
+    }
+    if (dut->perf_lsu_load_active)
+    {
+        ++load_store_unit_load_active_cycle_count;
+    }
+    if (dut->perf_lsu_store_active)
+    {
+        ++load_store_unit_store_active_cycle_count;
+    }
+    if (dut->perf_lsu_stall_read_ar)
+    {
+        ++lsu_stall_read_ar_count;
+    }
+    if (dut->perf_lsu_stall_read_r)
+    {
+        ++lsu_stall_read_r_count;
+    }
+    if (dut->perf_lsu_stall_write_req)
+    {
+        ++lsu_stall_write_req_count;
+    }
+    if (dut->perf_lsu_stall_write_b)
+    {
+        ++lsu_stall_write_b_count;
+    }
 #endif
 #ifdef CONFIG_ITRACE
     Iringbuf.push(dut->debug_pc, dut->debug_instructions, 4);
