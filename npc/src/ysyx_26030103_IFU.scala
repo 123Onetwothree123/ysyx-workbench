e2052ba35b72542439668ac110ba659530af35fd
tracer-ysyx <tracer@ysyx.org>
2026-07-20 18:54:34 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  18:54:34 up  9:55,  1 user,  load average: 1.67, 1.96, 1.61

--- npc/src/ysyx_26030103_IFU.scala
+++ npc/src/ysyx_26030103_IFU.scala
@@ -2,93 +2,127 @@ package ysyx_26030103
 import chisel3._
 import chisel3.util._
 import _root_.ysyx_26030103.ysyx_26030103_Message.ysyx_26030103_IFUMessage
-
+import _root_.ysyx_26030103.ysyx_26030103_AXI5._
 class ysyx_26030103_IFU(resetAddr: Long = 0x30000000L) extends Module {
   val io = IO(new Bundle {
-    val FetchAddr  = Output(UInt(32.W))
-    val FetchValid = Output(Bool())
-    val FetchReady = Input(Bool())
-
-    val RespData  = Input(UInt(32.W))
-    val RespValid = Input(Bool())
-    val RespReady = Output(Bool())
-
-    val RedirectTarget  = Input(UInt(32.W))
-    val Redirect        = Input(Bool())
-    val ExceptionTaken  = Input(Bool())
+    // ok啊，终于把AXI5总线干出来了，这下这个接口他妈的可以直接连AXI了
+    val InstructionBus = new ysyx_26030103_AXI5IO(32)
+    val RedirectTarget = Input(UInt(32.W)) // 跳转地址
+    val Redirect = Input(Bool())
+    val ExceptionTaken = Input(Bool()) // 异常或者是中断的信号
     val ExceptionTarget = Input(UInt(32.W))
-
-    val out = Decoupled(new ysyx_26030103_IFUMessage)
-
-    val DebugPC           = Output(UInt(32.W))
+    val out = Decoupled(new ysyx_26030103_IFUMessage) // 丢给ysyx_26030103_IDU
+    val DebugPC = Output(UInt(32.W))
     val DebugInstructions = Output(UInt(32.W))
-    val AccessFault       = Output(Bool())
-    val AccessFaultResp   = Output(UInt(2.W))
-    val StallPipeline     = Output(Bool())
-    val StallICache       = Output(Bool())
-    val StallIdle         = Output(Bool())
+    val AccessFault = Output(Bool())
+    val AccessFaultResp = Output(UInt(2.W))
+    val StallPipeline = Output(Bool())
+    val StallAXI = Output(Bool())
+    val StallAR = Output(Bool())
+    val StallR  = Output(Bool())
+    val StallIdle = Output(Bool())
   })
-  val PCModule     = Module(new ysyx_26030103_PC(resetAddr))
+  val PCModule = Module(new ysyx_26030103_PC(resetAddr))
   val NextPCModule = Module(new ysyx_26030103_NextPC)
   val snpc = PCModule.io.ysyx_26030103_PC + 4.U(32.W)
-
-  val states = Enum(3)
-  val StatesIdle       = states(0)
-  val StatesWaitICache = states(1)
-  val StatesHold       = states(2)
+  // 状态机，我真的是服了，谁能想到wait是scala和Java的object的内置方法，居然还不能用这个做变量名
+  val states = Enum(4)
+  val StatesIdle = states(0)
+  val StatesWaitRequest = states(1)
+  val StatesWaitResponse = states(2)
+  val StatesHold = states(3)
   val state = RegInit(StatesIdle)
-
-  val InstructionReg     = RegInit(0.U(32.W))
-  val PCReg              = RegInit(0.U(32.W))
-  val AccessFaultReg     = RegInit(false.B)
+  val InstructionReg = RegInit(0.U(32.W))
+  val PCReg = RegInit(0.U(32.W))
+  val AccessFaultReg = RegInit(false.B)
   val AccessFaultRespReg = RegInit(0.U(2.W))
-
-  io.AccessFault     := AccessFaultReg
+  io.AccessFault := AccessFaultReg
   io.AccessFaultResp := AccessFaultRespReg
-
-  io.FetchValid := false.B
-  io.FetchAddr  := 0.U
-  io.RespReady  := false.B
-  io.out.valid  := false.B
+  val InstructionResponseFire =
+    io.InstructionBus.R.RVALID && io.InstructionBus.R.RREADY
+  // 他妈的居然因为要防止报警转错，还要手动去给个初始值，哪个天才做的编译器
+  // 写通道全关，ysyx_26030103_IFU只读不写
+  io.InstructionBus.AW.AWVALID := false.B
+  io.InstructionBus.AW.AWID := 0.U
+  io.InstructionBus.AW.AWADDR := 0.U
+  io.InstructionBus.AW.AWLEN := 0.U
+  io.InstructionBus.AW.AWSIZE := 2.U
+  io.InstructionBus.AW.AWBURST := 1.U
+  io.InstructionBus.AW.AWPROT := 0.U
+  io.InstructionBus.W.WVALID := false.B
+  io.InstructionBus.W.WDATA := 0.U
+  io.InstructionBus.W.WSTRB := 0.U
+  io.InstructionBus.W.WLAST := false.B
+  io.InstructionBus.B.BREADY := false.B
+//只有这些用得到了
+  io.InstructionBus.AR.ARVALID := false.B
+  io.InstructionBus.AR.ARID := 0.U
+  io.InstructionBus.AR.ARADDR := 0.U
+  io.InstructionBus.AR.ARLEN := 0.U
+  io.InstructionBus.AR.ARSIZE := 2.U
+  io.InstructionBus.AR.ARBURST := 1.U
+  io.InstructionBus.AR.ARPROT := 0.U
+  io.InstructionBus.R.RREADY := false.B
+  io.out.valid := false.B
   io.out.bits.Instruction := InstructionReg
-  io.out.bits.pc          := PCReg
-
+  io.out.bits.pc := PCReg
   switch(state) {
     is(StatesIdle) {
-      AccessFaultReg     := false.B
+      AccessFaultReg := false.B
       AccessFaultRespReg := 0.U
-      io.FetchValid := true.B
-      io.FetchAddr  := PCModule.io.ysyx_26030103_PC
-      PCReg         := PCModule.io.ysyx_26030103_PC
-      when(io.FetchReady) {
-        state := StatesWaitICache
+      io.InstructionBus.AR.ARVALID := true.B
+      io.InstructionBus.AR.ARADDR := PCModule.io.ysyx_26030103_PC
+      PCReg := PCModule.io.ysyx_26030103_PC
+      state := Mux(
+        io.InstructionBus.AR.ARREADY,
+        StatesWaitResponse,
+        StatesWaitRequest
+      )
+    }
+    is(StatesWaitRequest) {
+      io.InstructionBus.AR.ARVALID := true.B
+      io.InstructionBus.AR.ARADDR := PCModule.io.ysyx_26030103_PC
+      when(io.InstructionBus.AR.ARREADY) {
+        state := StatesWaitResponse
       }
     }
-    is(StatesWaitICache) {
-      io.RespReady := true.B
-      when(io.RespValid && io.RespReady) {
-        InstructionReg := io.RespData
+    is(StatesWaitResponse) {
+      io.InstructionBus.R.RREADY := true.B
+      when(InstructionResponseFire) {
+        when(io.InstructionBus.R.RRESP =/= 0.U) {
+          AccessFaultReg := true.B
+          AccessFaultRespReg := io.InstructionBus.R.RRESP
+        }.otherwise {
+          InstructionReg := io.InstructionBus.R.RDATA
+        }
         state := StatesHold
       }
     }
     is(StatesHold) {
       io.out.valid := true.B
-      when(io.out.valid && io.out.ready) {
+      when(
+        io.out.valid && io.out.ready
+      ) { // 只有ysyx_26030103_IDU真的接走了这条指令，ysyx_26030103_IFU这里才会去进入下一次的取指
         state := StatesIdle
       }
     }
   }
-  NextPCModule.io.SNPC             := snpc
-  NextPCModule.io.Redirect         := io.Redirect
-  NextPCModule.io.RedirectTarget   := io.RedirectTarget
-  NextPCModule.io.ExceptionTaken   := io.ExceptionTaken
-  NextPCModule.io.ExceptionTarget  := io.ExceptionTarget
+  NextPCModule.io.SNPC := snpc
+  NextPCModule.io.Redirect := io.Redirect
+  NextPCModule.io.RedirectTarget := io.RedirectTarget
+  NextPCModule.io.ExceptionTaken := io.ExceptionTaken
+  NextPCModule.io.ExceptionTarget := io.ExceptionTarget
+//最开始还是按Verilog的版本写法照抄下来的，后面发现可以这么写，最开始不这么写是怕这样的话，后面的单元不是还没跑完就ysyx_26030103_PC就迭代了
+//后面发现写使能可以直接等时钟周期后再更新。
+//他妈的，vsc插件的格式化文档居然抽风了，独立行没法格式化，不是，自己做的东西，自己没跑过吗
   PCModule.io.ysyx_26030103_NextPC := NextPCModule.io.ysyx_26030103_NextPC
   PCModule.io.PCEnable := NextPCModule.io.PCEnable && io.out.fire
-
-  io.DebugPC           := PCModule.io.ysyx_26030103_PC
+//sdb
+  io.DebugPC := PCModule.io.ysyx_26030103_PC
   io.DebugInstructions := InstructionReg
-  io.StallPipeline     := state === StatesHold
-  io.StallICache       := state === StatesWaitICache
-  io.StallIdle         := state === StatesIdle
+  io.StallPipeline := state === StatesHold
+  io.StallAXI := state === StatesWaitRequest || state === StatesWaitResponse
+  io.StallAR := state === StatesWaitRequest
+  io.StallR  := state === StatesWaitResponse
+  io.StallIdle := state === StatesIdle
 }
