eebebc1d2103da1deadc0e07a5a52f5c0aa6baf0
tracer-ysyx <tracer@ysyx.org>
2026-07-20 16:01:29 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  16:01:29 up  7:02,  1 user,  load average: 2.73, 2.12, 1.45

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -54,7 +54,6 @@ int main(int argc, char const *argv[])
 #ifdef CONFIG_SDB
     SDB::MainLoop(dut);
 #else
-    std::println(stderr, "[NPC-DEBUG] entering loop");
     while (!Verilated::gotFinish() && !NPCTrap::HasHalted())
     {
         dut.step();
