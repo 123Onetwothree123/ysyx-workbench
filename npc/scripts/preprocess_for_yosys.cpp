dcf5032c35168dbb7a77d4c7d5ae50f7f8ef962b
tracer-ysyx <tracer@ysyx.org>
2026-07-20 17:37:51 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  17:37:51 up  8:38,  1 user,  load average: 0.88, 0.84, 1.03

--- npc/scripts/preprocess_for_yosys.cpp
+++ npc/scripts/preprocess_for_yosys.cpp
@@ -189,6 +189,7 @@ int main(int argc, char *argv[])
                 auto expr{bbm[1].str()};
                 while (!expr.empty() && expr.front() == ' ') expr.erase(0, 1);
                 while (!expr.empty() && expr.back() == ' ') expr.pop_back();
+                if (!expr.empty() && expr.front() == '{') expr.erase(0, 1);
                 wires.push_back(std::format("wire [{}:0] {} = {};", hi - lo, tn, expr));
                 auto repl{std::format("{}[{}:{}]", tn, hi, lo)};
                 cleaned.push_back(std::regex_replace(ri, brace_bit, repl));
