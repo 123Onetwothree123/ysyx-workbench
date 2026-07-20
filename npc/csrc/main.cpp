a6fa4293b023916944586194069da9d3d3ac9b9f
tracer-ysyx <tracer@ysyx.org>
2026-07-20 16:15:20 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  16:15:20 up  7:15,  1 user,  load average: 2.99, 2.48, 1.93

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -66,78 +66,76 @@ int main(int argc, char const *argv[])
             std::println("trap了");
             const auto halt_code{dut.ReadGPR(10)}; // x10 = a0
             NPCTrap::Halt(static_cast<std::uint32_t>(dut->trap_pc), halt_code ? *halt_code : 1u);
-            result = NPCTrap::PrintResult(dut.GetCycle(), dut.GetInstructions());
-#ifdef CONFIG_PERF_STATS
-            NPCTrap::PrintPerformanceStatistics(
-                dut.GetInstructionFetchCount(),
-                dut.GetExecutionCompleteCount(),
-                dut.GetLoadDataCount(),
-                dut.GetStoreDataCount(),
-                dut.GetArithmeticOperationCount(),
-                dut.GetMemoryAccessOperationCount(),
-                dut.GetControlStatusRegisterOperationCount(),
-                dut.GetBranchOperationCount(),
-                dut.GetCycle(),
-                dut.GetInstructionFetchStallPipelineCount(),
-                dut.GetInstructionFetchStallAXICount(),
-                dut.GetInstructionFetchStallRedirectCount(),
-                dut.GetInstructionFetchStallARCount(),
-                dut.GetInstructionFetchStallRCount(),
-                dut.GetInstructionFetchStallIdleCount(),
-                dut.GetArithmeticOperationActiveCycleCount(),
-                dut.GetMemoryAccessOperationActiveCycleCount(),
-                dut.GetControlStatusRegisterOperationActiveCycleCount(),
-                dut.GetBranchOperationActiveCycleCount(),
-                dut.GetEXUStallLSUCount(),
-                dut.GetLoadStoreUnitActiveCycleCount(),
-                dut.GetLoadStoreUnitLoadActiveCycleCount(),
-                dut.GetLoadStoreUnitStoreActiveCycleCount(),
-                dut.GetLSUStallReadARCount(),
-                dut.GetLSUStallReadRCount(),
-                dut.GetLSUStallWriteReqCount(),
-                dut.GetLSUStallWriteBCount(),
-                dut.GetICacheHitCount(),
-                dut.GetICacheMissCount());
-#endif
-#ifdef CONFIG_PERF_SAVE
-            NPCSimResult::Save(
-                options->GetResultDir().value_or("NPCSimResult"),
-                dut.GetCycle(),
-                dut.GetInstructions(),
-                dut.GetInstructionFetchCount(),
-                dut.GetExecutionCompleteCount(),
-                dut.GetLoadDataCount(),
-                dut.GetStoreDataCount(),
-                dut.GetArithmeticOperationCount(),
-                dut.GetMemoryAccessOperationCount(),
-                dut.GetControlStatusRegisterOperationCount(),
-                dut.GetBranchOperationCount(),
-                dut.GetMemoryAccessOperationActiveCycleCount(),
-                dut.GetInstructionFetchStallPipelineCount(),
-                dut.GetInstructionFetchStallAXICount(),
-                dut.GetInstructionFetchStallARCount(),
-                dut.GetInstructionFetchStallRCount(),
-                dut.GetInstructionFetchStallRedirectCount(),
-                dut.GetInstructionFetchStallIdleCount(),
-                dut.GetEXUStallLSUCount(),
-                dut.GetLoadStoreUnitActiveCycleCount(),
-                dut.GetLoadStoreUnitLoadActiveCycleCount(),
-                dut.GetLoadStoreUnitStoreActiveCycleCount(),
-                dut.GetLSUStallReadARCount(),
-                dut.GetLSUStallReadRCount(),
-                dut.GetLSUStallWriteReqCount(),
-                dut.GetLSUStallWriteBCount(),
-                dut.GetICacheHitCount(),
-                dut.GetICacheMissCount());
-#endif
-            break;
         }
     }
-    std::println("[NPC] loop exited, halting={}", NPCTrap::HasHalted());
 #endif
     dut.final();
 #ifdef CONFIG_NVBOARD
     nvboard_quit();
+#endif
+    result = NPCTrap::PrintResult(dut.GetCycle(), dut.GetInstructions());
+#ifdef CONFIG_PERF_STATS
+    NPCTrap::PrintPerformanceStatistics(
+        dut.GetInstructionFetchCount(),
+        dut.GetExecutionCompleteCount(),
+        dut.GetLoadDataCount(),
+        dut.GetStoreDataCount(),
+        dut.GetArithmeticOperationCount(),
+        dut.GetMemoryAccessOperationCount(),
+        dut.GetControlStatusRegisterOperationCount(),
+        dut.GetBranchOperationCount(),
+        dut.GetCycle(),
+        dut.GetInstructionFetchStallPipelineCount(),
+        dut.GetInstructionFetchStallAXICount(),
+        dut.GetInstructionFetchStallRedirectCount(),
+        dut.GetInstructionFetchStallARCount(),
+        dut.GetInstructionFetchStallRCount(),
+        dut.GetInstructionFetchStallIdleCount(),
+        dut.GetArithmeticOperationActiveCycleCount(),
+        dut.GetMemoryAccessOperationActiveCycleCount(),
+        dut.GetControlStatusRegisterOperationActiveCycleCount(),
+        dut.GetBranchOperationActiveCycleCount(),
+        dut.GetEXUStallLSUCount(),
+        dut.GetLoadStoreUnitActiveCycleCount(),
+        dut.GetLoadStoreUnitLoadActiveCycleCount(),
+        dut.GetLoadStoreUnitStoreActiveCycleCount(),
+        dut.GetLSUStallReadARCount(),
+        dut.GetLSUStallReadRCount(),
+        dut.GetLSUStallWriteReqCount(),
+        dut.GetLSUStallWriteBCount(),
+        dut.GetICacheHitCount(),
+        dut.GetICacheMissCount());
+#endif
+#ifdef CONFIG_PERF_SAVE
+    NPCSimResult::Save(
+        options->GetResultDir().value_or("NPCSimResult"),
+        dut.GetCycle(),
+        dut.GetInstructions(),
+        dut.GetInstructionFetchCount(),
+        dut.GetExecutionCompleteCount(),
+        dut.GetLoadDataCount(),
+        dut.GetStoreDataCount(),
+        dut.GetArithmeticOperationCount(),
+        dut.GetMemoryAccessOperationCount(),
+        dut.GetControlStatusRegisterOperationCount(),
+        dut.GetBranchOperationCount(),
+        dut.GetMemoryAccessOperationActiveCycleCount(),
+        dut.GetInstructionFetchStallPipelineCount(),
+        dut.GetInstructionFetchStallAXICount(),
+        dut.GetInstructionFetchStallARCount(),
+        dut.GetInstructionFetchStallRCount(),
+        dut.GetInstructionFetchStallRedirectCount(),
+        dut.GetInstructionFetchStallIdleCount(),
+        dut.GetEXUStallLSUCount(),
+        dut.GetLoadStoreUnitActiveCycleCount(),
+        dut.GetLoadStoreUnitLoadActiveCycleCount(),
+        dut.GetLoadStoreUnitStoreActiveCycleCount(),
+        dut.GetLSUStallReadARCount(),
+        dut.GetLSUStallReadRCount(),
+        dut.GetLSUStallWriteReqCount(),
+        dut.GetLSUStallWriteBCount(),
+        dut.GetICacheHitCount(),
+        dut.GetICacheMissCount());
 #endif
 #ifdef CONFIG_DIFFTEST
     if (result != 0)
