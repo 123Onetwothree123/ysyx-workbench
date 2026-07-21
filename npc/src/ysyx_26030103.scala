db3c652a54b360c24775817b9ae1e57d4574bff6
tracer-ysyx <tracer@ysyx.org>
2026-07-21 13:09:36 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  13:09:36 up  3:54,  1 user,  load average: 2.52, 3.10, 2.28

--- npc/src/ysyx_26030103.scala
+++ npc/src/ysyx_26030103.scala
@@ -20,7 +20,7 @@ class ysyx_26030103(
   val lsu = Module(new ysyx_26030103_LSU)
   val gpr = Module(new ysyx_26030103_GPR)
   val icache = Module(new ysyx_26030103_ICache(
-    BlockSizeLog2 = 2, CacheableBase = CacheableBase,
+    BlockSizeLog2 = 4, CacheableBase = CacheableBase,
     CacheableMask = CacheableMask
   ))
   val arbiter = Module(new ysyx_26030103_AXI5Arbiter)
