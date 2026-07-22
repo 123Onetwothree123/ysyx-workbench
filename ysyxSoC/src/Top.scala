17f87c539ee6e6fc412aeabe2cfc97e21f970eb4
tracer-ysyx <tracer@ysyx.org>
2026-07-22 16:34:37 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  16:34:37 up  6:52,  1 user,  load average: 2.15, 3.20, 2.86

--- ysyxSoC/src/Top.scala
+++ ysyxSoC/src/Top.scala
@@ -8,7 +8,7 @@ import freechips.rocketchip.diplomacy.LazyModule
 object Config {
   //def hasChipLink: Boolean = true
   def hasChipLink: Boolean = false
-  def sdramUseAXI: Boolean = true
+  def sdramUseAXI: Boolean = false
 }
 
 class ysyxSoCTop extends Module {
