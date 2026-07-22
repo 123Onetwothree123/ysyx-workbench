5daedd128e61a47b2cdee4a5e34ef36b55056f7e
tracer-ysyx <tracer@ysyx.org>
2026-07-22 16:28:06 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  16:28:06 up  6:45,  1 user,  load average: 3.10, 2.51, 2.39

--- npc/csrc/ysyxSoC/ysyxSoC.cpp
+++ npc/csrc/ysyxSoC/ysyxSoC.cpp
@@ -1,4 +1,5 @@
 module;
+#include <cstdio>
 module npc.ysyxSoC;
 #ifndef CONFIG_MBASE
 #define CONFIG_MBASE 0x20000000u
@@ -21,6 +22,11 @@ extern "C" void flash_read(std::int32_t addr, std::int32_t *data)
 }
 extern "C" void mrom_read(std::int32_t addr, std::int32_t *data)
 {
+    static int first = 1;
+    if (first) {
+        printf("[MROM] first read: addr=0x%x mrom.size=%zu\n", (unsigned)addr, mrom.size());
+        first = 0;
+    }
     std::uint32_t offset{static_cast<std::uint32_t>(addr) - static_cast<std::uint32_t>(CONFIG_MBASE)}; // 脑残编译器，这里auto会解读成ui32，他妈的那每次auto写了后，还得检查一遍，这和没写auto有什么区别，他妈的
     std::uint32_t aligned{offset & ~3u};
     std::uint32_t result{0};
