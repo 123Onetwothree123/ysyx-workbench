8b676365f883ac67cd8ac921ceb1d7a8c952c9a3
tracer-ysyx <tracer@ysyx.org>
2026-07-22 21:02:03 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  21:02:03 up 11:19,  1 user,  load average: 0.80, 1.57, 1.66

--- npc/csrc/ysyxSoC/ysyxSoC.cpp
+++ npc/csrc/ysyxSoC/ysyxSoC.cpp
@@ -33,8 +33,8 @@ extern "C" void mrom_read(std::int32_t addr, std::int32_t *data)
 }
 extern "C" void axi_debug_probe(std::int32_t tag, std::int32_t addr, std::int32_t resp)
 {
-    static const char *names[] = {"ICACHE", "LSU_R", "LSU_B", "SOC_R", "SOC_B", "GPR_W", "WBU_W", "IFU_F"};
+    static const char *names[] = {"ICACHE", "LSU_R", "LSU_B", "SOC_R", "SOC_B", "GPR_W", "WBU_W", "IFU_F", "IC_RESP"};
     auto idx{static_cast<unsigned>(tag)};
-    auto name{(idx < 8) ? names[idx] : "UNKNOWN"};
+    auto name{(idx < 9) ? names[idx] : "UNKNOWN"};
     std::println(std::cerr, "[AXI_DEBUG] {} addr=0x{:08x} data=0x{:08x}", name, static_cast<std::uint32_t>(addr), static_cast<std::uint32_t>(resp));
 }
