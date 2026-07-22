1b772b5cf404de944c0f282f6279631d0290d712
tracer-ysyx <tracer@ysyx.org>
2026-07-22 12:04:36 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  12:04:36 up  2:22,  1 user,  load average: 1.19, 1.92, 2.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -53,6 +53,9 @@ int main(int argc, char const *argv[])
     }
 #endif
     dut.reset();
+    std::println("reset done, commit={}, pc=0x{:08x}",
+        static_cast<int>(dut->debug_commit),
+        static_cast<std::uint32_t>(dut->debug_pc));
 #ifdef CONFIG_SDB
     SDB::MainLoop(dut);
 #else
