25b31291787cc0a8df8b617a15ffc9fde3856aea
tracer-ysyx <tracer@ysyx.org>
2026-07-24 23:09:45 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  23:09:45 up 14:23,  1 user,  load average: 3.59, 3.98, 2.92

--- npc/src/ysyx_26030103_ICache.scala
+++ npc/src/ysyx_26030103_ICache.scala
@@ -107,7 +107,7 @@ class ysyx_26030103_ICache(
     is(state_refill_req) {
       io.axi.AR.ARVALID := true.B
       io.axi.AR.ARADDR := Mux(cacheable_reg,
-        Cat(fetch_addr_reg(AddressWidth - 1, BlockSizeLog2), refill_cnt, 0.U(2.W)),
+        Cat(fetch_addr_reg(AddressWidth - 1, BlockSizeLog2), 0.U((BlockSizeLog2).W)),
         fetch_addr_reg
       )
       when(io.axi.AR.ARREADY) {
@@ -120,21 +120,21 @@ class ysyx_26030103_ICache(
         when(io.axi.R.RRESP =/= 0.U) {
           access_fault_reg := true.B
           access_fault_resp_reg := io.axi.R.RRESP
+          state := state_resp
         }.otherwise {
           resp_data_reg := io.axi.R.RDATA
           when(cacheable_reg) {
             tag(fetch_index_reg)   := fetch_tag_reg
             data(fetch_index_reg)(refill_cnt) := io.axi.R.RDATA
-            when(refill_cnt === (WordsPerBlock - 1).U) {
-              valid(fetch_index_reg) := true.B  // 全部word填完才设valid
-            }
           }
-        }
-        when(refill_cnt === (WordsPerBlock - 1).U || !cacheable_reg) {
-          state := state_resp
-        }.otherwise {
-          refill_cnt := refill_cnt + 1.U
-          state := state_refill_req
+          when(io.axi.R.RLAST || !cacheable_reg) {
+            when(cacheable_reg) {
+              valid(fetch_index_reg) := true.B
+            }
+            state := state_resp
+          }.otherwise {
+            refill_cnt := refill_cnt + 1.U
+          }
         }
       }
     }
