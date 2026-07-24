2c1aed59b478a904035eccde81a3dc96e8f7c17f
tracer-ysyx <tracer@ysyx.org>
2026-07-24 10:06:23 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  10:06:23 up  1:19,  1 user,  load average: 1.06, 1.86, 2.09

--- ysyxSoC/perip/uart16550/rtl/uart_regs.v
+++ ysyxSoC/perip/uart16550/rtl/uart_regs.v
@@ -447,6 +447,11 @@ assign msr_read = (wb_re_i && wb_addr_i == `UART_REG_MS && !dlab);
 assign fifo_read = (wb_re_i && wb_addr_i == `UART_REG_RB && !dlab);
 assign fifo_write = (wb_we_i && wb_addr_i == `UART_REG_TR && !dlab);
 
+// simulation: capture UART TX byte output
+`ifdef VERILATOR
+always @(posedge clk) if (fifo_write) $write("%c", wb_dat_i[7:0]);
+`endif
+
 // lsr_mask_d delayed signal handling
 always @(posedge clk or posedge wb_rst_i)
 begin
