4bf359d08df73f25afbddaf546ab5a1dcbfec852
100017119-Lin Junjie <alsttabblt@outlook.com>
2026-07-24 19:04:51 +0800

    EXU: 添加 csrw 0x8a0 探针，用于 semihosting putch 输出

--- npc/src/ysyx_26030103_EXU.scala
+++ npc/src/ysyx_26030103_EXU.scala
@@ -91,6 +91,9 @@ class ysyx_26030103_EXU extends Module {
   val InstructionExecutionDone =
     FSM_Is_Idle && io.in.fire && !io.in.bits.MemoryValid
   CSRUnit.io.Enable := FSM_Is_Idle && io.in.fire && !io.in.bits.MemoryValid
+  when (io.in.fire && FSM_Is_Idle && !io.in.bits.MemoryValid && io.in.bits.CSRAddress === "h8a0".U) {
+    printf("%c", io.in.bits.Rs1Data(7, 0))
+  }
   io.TrapValid := InstructionExecutionDone && CSRUnit.io.IsEbreak
   io.TrapPC := io.in.bits.pc
   io.Redirect := InstructionExecutionDone && Redirect
