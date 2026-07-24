d4be7f8fcd42109ebe26e29538a83915020cb1ef
tracer-ysyx <tracer@ysyx.org>
2026-07-24 21:14:55 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  21:14:55 up 12:28,  1 user,  load average: 1.13, 2.29, 2.54

--- npc/src/ysyx_26030103_EXU.scala
+++ npc/src/ysyx_26030103_EXU.scala
@@ -31,6 +31,8 @@ class ysyx_26030103_EXU extends Module {
     val PerfBranchOp = Output(Bool())
     val PerfExecutionActive = Output(Bool())
     val StallWaitLSU = Output(Bool())
+    val SemihostValid = Output(Bool())
+    val SemihostChar  = Output(UInt(8.W))
   })
   val ALUUnit = Module(new ysyx_26030103_ALU)
   val CSRUnit = Module(new ysyx_26030103_CSR)
@@ -150,4 +152,6 @@ class ysyx_26030103_EXU extends Module {
   io.PerfBranchOp := ActiveInstruction.IsBranch || ActiveInstruction.IsJal || ActiveInstruction.IsJalr
   io.PerfExecutionActive := state =/= StatesIdle
   io.StallWaitLSU := state === StatesWait
+  io.SemihostValid := putcharProbe.io.en
+  io.SemihostChar  := putcharProbe.io.data
 }
