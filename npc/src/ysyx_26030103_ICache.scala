0f0cbb0129353367437ccb845f0cad48989f7dff
tracer-ysyx <tracer@ysyx.org>
2026-07-25 21:09:40 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  21:09:40 up 12:05,  1 user,  load average: 1.19, 1.99, 1.74

--- npc/src/ysyx_26030103_ICache.scala
+++ npc/src/ysyx_26030103_ICache.scala
@@ -107,7 +107,7 @@ class ysyx_26030103_ICache(
     }
     is(state_refill_req) {
       io.axi.AR.ARVALID := true.B
-      io.axi.AR.ARLEN := Mux(cacheable_reg, (WordsPerBlock - 1).U, 0.U)
+      io.axi.AR.ARLEN := 0.U
       io.axi.AR.ARADDR := Mux(cacheable_reg,
         Cat(fetch_addr_reg(AddressWidth - 1, BlockSizeLog2), 0.U(BlockSizeLog2.W)),
         fetch_addr_reg
