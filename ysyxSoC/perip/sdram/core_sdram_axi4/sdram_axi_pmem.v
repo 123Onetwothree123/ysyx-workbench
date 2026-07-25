4378161256fe3dc7fd77e319075ffce70d01f386
100017119-Lin Junjie <alsttabblt@outlook.com>
2026-07-25 10:20:19 +0800

    清理 SDRAM pmem 调试探针

--- ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_pmem.v
+++ ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_pmem.v
@@ -347,11 +347,6 @@ assign resp_accept_w    = (axi_rvalid_o & axi_rready_i) |
                           (resp_valid_w & resp_is_write_w & !resp_is_last_w) |
                           (resp_valid_w & resp_is_write_w & resp_is_last_w & wr_b_timeout);
 
-always @(posedge clk_i) if(axi_arvalid_i & axi_arready_o) $display("[P] AR len=%d", axi_arlen_i);
-always @(posedge clk_i) if(axi_rvalid_o & axi_rready_i) $display("[P] R last=%d", axi_rlast_o);
-reg [31:0] _d;
-always @(posedge clk_i or posedge rst_i) if(rst_i) _d<=0; else _d<=_d+1;
-always @(posedge clk_i) if(axi_rvalid_o & axi_rready_i) $display("[P] R c=%d last=%d", _d, axi_rlast_o);
 endmodule
 
 //-----------------------------------------------------------------
