bcde55122410baf5f182b9970adcb76e376cfb87
tracer-ysyx <tracer@ysyx.org>
2026-07-25 14:01:52 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  14:01:52 up  4:57,  1 user,  load average: 1.41, 2.01, 2.98

--- ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_pmem.v
+++ ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_pmem.v
@@ -148,8 +148,8 @@ begin
 end
 else
 begin
-    // Burst continuation (only if already in burst)
-    if ((ram_wr_o != 4'b0 || ram_rd_o) && ram_accept_i && (req_rd_q || req_wr_q))
+    // Burst continuation
+    if ((ram_wr_o != 4'b0 || ram_rd_o) && ram_accept_i)
     begin
         if (req_len_q == 8'd0)
         begin
@@ -163,8 +163,33 @@ begin
         end
     end
 
+    // Write command accepted
+    if (axi_awvalid_i && axi_awready_o)
+    begin
+        // Data ready?
+        if (axi_wvalid_i && axi_wready_o)
+        begin
+            req_wr_q      <= 1'b1;
+            req_len_q     <= axi_awlen_i;
+            req_id_q      <= axi_awid_i;
+            req_axburst_q <= axi_awburst_i;
+            req_axlen_q   <= axi_awlen_i;
+            req_addr_q    <= axi_awaddr_i;
+        end
+        // Data not ready
+        else
+        begin
+            req_wr_q      <= 1'b1;
+            req_len_q     <= axi_awlen_i;
+            req_id_q      <= axi_awid_i;
+            req_axburst_q <= axi_awburst_i;
+            req_axlen_q   <= axi_awlen_i;
+            req_addr_q    <= axi_awaddr_i;
+        end
+        req_prio_q    <= !req_prio_q;
+    end
     // Read command accepted
-    if (axi_arvalid_i && axi_arready_o)
+    else if (axi_arvalid_i && axi_arready_o)
     begin
         req_rd_q      <= (axi_arlen_i != 0);
         req_len_q     <= axi_arlen_i;
@@ -172,17 +197,6 @@ begin
         req_id_q      <= axi_arid_i;
         req_axburst_q <= axi_arburst_i;
         req_axlen_q   <= axi_arlen_i;
-        req_prio_q    <= !req_prio_q;
-    end
-    // Write command accepted
-    else if (axi_awvalid_i && axi_awready_o)
-    begin
-        req_wr_q      <= 1'b1;
-        req_len_q     <= axi_awlen_i;
-        req_id_q      <= axi_awid_i;
-        req_axburst_q <= axi_awburst_i;
-        req_axlen_q   <= axi_awlen_i;
-        req_addr_q    <= axi_awaddr_i;
         req_prio_q    <= !req_prio_q;
     end
 end
@@ -209,8 +223,8 @@ end
 //-----------------------------------------------------------------
 // Request tracking
 //-----------------------------------------------------------------
+wire       req_push_w = (ram_rd_o || (ram_wr_o != 4'b0)) && ram_accept_i;
 reg [5:0]  req_in_r;
-reg        req_push_w;
 
 wire       req_out_valid_w;
 wire [5:0] req_out_w;
@@ -220,20 +234,16 @@ wire       resp_accept_w;
 always @ *
 begin
     req_in_r = 6'b0;
-    req_push_w = 1'b0;
 
-    if (axi_arvalid_i && axi_arready_o) begin
+    // First cycle of read burst
+    if (axi_arvalid_i && axi_arready_o)
         req_in_r = {1'b1, (axi_arlen_i == 8'd0), axi_arid_i};
-        req_push_w = (axi_arlen_i == 8'd0);
-    end
-    else if (axi_awvalid_i && axi_awready_o) begin
+    // First cycle of write burst
+    else if (axi_awvalid_i && axi_awready_o)
         req_in_r = {1'b0, (axi_awlen_i == 8'd0), axi_awid_i};
-        req_push_w = 1'b1;
-    end
-    else if ((ram_rd_o || (ram_wr_o != 4'b0)) && ram_accept_i) begin
+    // In burst
+    else
         req_in_r = {ram_rd_o, (req_len_q == 8'd0), req_id_q};
-        req_push_w = 1'b1;
-    end
 end
 
 sdram_axi_pmem_fifo2
@@ -336,7 +346,7 @@ assign resp_accept_w    = (axi_rvalid_o & axi_rready_i) |
                           (axi_bvalid_o & axi_bready_i) |
                           (resp_valid_w & resp_is_write_w & !resp_is_last_w) |
                           (resp_valid_w & resp_is_write_w & resp_is_last_w & wr_b_timeout);
-always @(posedge clk_i) if(axi_rvalid_o & axi_rready_i) $display("[P] R last=%d data=%08x", axi_rlast_o, axi_rdata_o);
+
 endmodule
 
 //-----------------------------------------------------------------
