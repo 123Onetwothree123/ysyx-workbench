c7dc6a6c052a36b803f72f9f27b97bc00b565cd9
tracer-ysyx <tracer@ysyx.org>
2026-07-25 12:06:49 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  12:06:49 up  3:02,  1 user,  load average: 0.77, 1.40, 1.90

--- npc/src/ysyx_26030103_ICache.scala
+++ npc/src/ysyx_26030103_ICache.scala
@@ -71,7 +71,7 @@ class ysyx_26030103_ICache(
   io.axi.AR.ARVALID := false.B
   io.axi.AR.ARID := 0.U
   io.axi.AR.ARADDR := 0.U
-  io.axi.AR.ARLEN := Mux(fetch_addr_reg(31), (WordsPerBlock - 1).U, 0.U)
+  io.axi.AR.ARLEN := 0.U
   io.axi.AR.ARSIZE := 2.U
   io.axi.AR.ARBURST := 1.U
   io.axi.AR.ARPROT := 0.U
@@ -88,17 +88,17 @@ class ysyx_26030103_ICache(
       access_fault_reg := false.B
       access_fault_resp_reg := 0.U
       io.fetch_ready := true.B
-      io.perf_hit  := io.fetch_valid && io.fetch_ready && io.fetch_addr(31).asBool && hit
-      io.perf_miss := io.fetch_valid && io.fetch_ready && io.fetch_addr(31).asBool && !hit
+      io.perf_hit  := io.fetch_valid && io.fetch_ready && cacheable && hit
+      io.perf_miss := io.fetch_valid && io.fetch_ready && cacheable && !hit
       when(io.fetch_valid && io.fetch_ready) {
         fetch_addr_reg  := io.fetch_addr
         fetch_index_reg := index
         fetch_tag_reg   := reqTag
         resp_data_reg   := data(index)(blockOffset)
-        cacheable_reg   := io.fetch_addr(31,31)
+        cacheable_reg   := cacheable
         fetch_offset_reg := blockOffset
         refill_cnt      := 0.U
-        when(io.fetch_addr(31).asBool && hit) {
+        when(cacheable && hit) {
           state := state_resp
         }.otherwise {
           state := state_refill_req
@@ -107,10 +107,11 @@ class ysyx_26030103_ICache(
     }
     is(state_refill_req) {
       io.axi.AR.ARVALID := true.B
-      io.axi.AR.ARADDR := Mux(fetch_addr_reg(31),
+      io.axi.AR.ARADDR := Mux(fetch_addr_reg(31).asBool,
         Cat(fetch_addr_reg(AddressWidth - 1, BlockSizeLog2), 0.U((BlockSizeLog2).W)),
         fetch_addr_reg
       )
+      io.axi.AR.ARLEN := Mux(fetch_addr_reg(31).asBool, (WordsPerBlock - 1).U, 0.U)
       when(io.axi.AR.ARREADY) {
         state := state_refill_resp
       }
@@ -123,25 +124,23 @@ class ysyx_26030103_ICache(
           access_fault_resp_reg := io.axi.R.RRESP
         }.otherwise {
           resp_data_reg := io.axi.R.RDATA
-          when(fetch_addr_reg(31)) {
-            tag(fetch_index_reg)   := fetch_tag_reg
+          when(fetch_addr_reg(31).asBool) {
             data(fetch_index_reg)(refill_cnt) := io.axi.R.RDATA
-            when(refill_cnt === (WordsPerBlock - 1).U) {
-              valid(fetch_index_reg) := true.B  // 全部word填完才设valid
+            tag(fetch_index_reg) := fetch_tag_reg
+            when(refill_cnt < (WordsPerBlock - 1).U) {
+              refill_cnt := refill_cnt + 1.U
             }
           }
         }
-        when(io.axi.R.RLAST || !fetch_addr_reg(31)) {
-          when(fetch_addr_reg(31)) { valid(fetch_index_reg) := true.B }
+        when(io.axi.R.RLAST || !fetch_addr_reg(31).asBool) {
+          when(fetch_addr_reg(31).asBool) { valid(fetch_index_reg) := true.B }
           state := state_resp
-        }.otherwise {
-          when(refill_cnt < (WordsPerBlock - 1).U) { refill_cnt := refill_cnt + 1.U }
         }
       }
     }
     is(state_resp) {
       io.resp_valid := true.B
-      io.resp_data := Mux(fetch_addr_reg(31) && !access_fault_reg,
+      io.resp_data := Mux(fetch_addr_reg(31).asBool && !access_fault_reg,
         data(fetch_index_reg)(fetch_offset_reg), resp_data_reg)
       when(io.resp_ready && fetch_addr_reg === "ha00000ec".U) {
         printf(cf"ICache resp: addr=${fetch_addr_reg}, data=${io.resp_data}\n")
