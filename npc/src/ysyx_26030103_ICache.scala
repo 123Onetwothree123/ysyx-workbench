ae4e43fb4cf6e18947627d6ede6e9d7656232055
tracer-ysyx <tracer@ysyx.org>
2026-07-25 09:44:07 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  09:44:07 up 39 min,  1 user,  load average: 2.90, 3.54, 3.11

--- npc/src/ysyx_26030103_ICache.scala
+++ npc/src/ysyx_26030103_ICache.scala
@@ -44,7 +44,6 @@ class ysyx_26030103_ICache(
     else 0.U
   val reqTag = io.fetch_addr(AddressWidth - 1, IndexBits + BlockSizeLog2)
   val hit = valid(index) && tag(index) === reqTag
-  val cacheable = (io.fetch_addr & CacheableMask.U) === CacheableBase.U
   val fetch_addr_reg = Reg(UInt(AddressWidth.W))
   val fetch_index_reg = Reg(UInt(IndexBits.W))
   val fetch_tag_reg = Reg(UInt(TagBits.W))
@@ -88,17 +87,17 @@ class ysyx_26030103_ICache(
       access_fault_reg := false.B
       access_fault_resp_reg := 0.U
       io.fetch_ready := true.B
-      io.perf_hit  := io.fetch_valid && io.fetch_ready && cacheable && hit
-      io.perf_miss := io.fetch_valid && io.fetch_ready && cacheable && !hit
+      io.perf_hit  := io.fetch_valid && io.fetch_ready && io.fetch_addr(31).asBool && hit
+      io.perf_miss := io.fetch_valid && io.fetch_ready && io.fetch_addr(31).asBool && !hit
       when(io.fetch_valid && io.fetch_ready) {
         fetch_addr_reg  := io.fetch_addr
         fetch_index_reg := index
         fetch_tag_reg   := reqTag
         resp_data_reg   := data(index)(blockOffset)
-        cacheable_reg   := cacheable
+        cacheable_reg   := io.fetch_addr(31,31).asBool
         fetch_offset_reg := blockOffset
         refill_cnt      := 0.U
-        when(cacheable && hit) {
+        when(io.fetch_addr(31).asBool && hit) {
           state := state_resp
         }.otherwise {
           state := state_refill_req
