e6a98816cd70b441a90e263e0f89da3408ab5dc0
tracer-ysyx <tracer@ysyx.org>
2026-07-25 13:33:16 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  13:33:16 up  4:29,  1 user,  load average: 3.38, 2.90, 2.85

--- ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_pmem.v
+++ ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_pmem.v
@@ -336,6 +336,7 @@ assign resp_accept_w    = (axi_rvalid_o & axi_rready_i) |
                           (axi_bvalid_o & axi_bready_i) |
                           (resp_valid_w & resp_is_write_w & !resp_is_last_w) |
                           (resp_valid_w & resp_is_write_w & resp_is_last_w & wr_b_timeout);
+always @(posedge clk_i) if(axi_rvalid_o & axi_rready_i) $display("[P] R last=%d data=%08x", axi_rlast_o, axi_rdata_o);
 endmodule
 
 //-----------------------------------------------------------------
