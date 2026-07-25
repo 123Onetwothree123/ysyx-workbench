e8c07e27f2f7179135c896d927e4712deabdd364
tracer-ysyx <tracer@ysyx.org>
2026-07-25 16:17:24 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  16:17:24 up  7:13,  1 user,  load average: 3.04, 3.21, 4.40

--- ysyxSoC/rocket-chip/src/main/scala/amba/axi4/Xbar.scala
+++ ysyxSoC/rocket-chip/src/main/scala/amba/axi4/Xbar.scala
@@ -297,7 +297,7 @@ object AXI4Arbiter
     when (sink.fire) { idle := true.B }
 
     if (sources.size > 1) {
-      val allowed = Mux(idle, VecInit.fill(sources.size)(true.B), state)
+      val allowed = Mux(idle, readys, state)
       (sources zip allowed) foreach { case (s, r) =>
         s.ready := sink.ready && r
       }
