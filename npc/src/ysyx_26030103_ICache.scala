5732deaa72f8fcd5340a02040b46c63d869d0788
tracer-ysyx <tracer@ysyx.org>
2026-07-25 13:09:38 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  13:09:38 up  4:05,  1 user,  load average: 2.20, 3.34, 3.08

--- npc/src/ysyx_26030103_ICache.scala
+++ npc/src/ysyx_26030103_ICache.scala
@@ -108,10 +108,10 @@ class ysyx_26030103_ICache(
     is(state_refill_req) {
       io.axi.AR.ARVALID := true.B
       io.axi.AR.ARADDR := Mux(fetch_addr_reg(31).asBool,
-        Cat(fetch_addr_reg(AddressWidth - 1, BlockSizeLog2), refill_cnt, 0.U(2.W)),
+        Cat(fetch_addr_reg(AddressWidth - 1, BlockSizeLog2), 0.U((BlockSizeLog2).W)),
         fetch_addr_reg
       )
-      io.axi.AR.ARLEN := 0.U
+      io.axi.AR.ARLEN := Mux(fetch_addr_reg(31).asBool, (WordsPerBlock - 1).U, 0.U)
       when(io.axi.AR.ARREADY) {
         state := state_refill_resp
       }
@@ -125,18 +125,18 @@ class ysyx_26030103_ICache(
         }.otherwise {
           resp_data_reg := io.axi.R.RDATA
           when(fetch_addr_reg(31).asBool) {
-            tag(fetch_index_reg)   := fetch_tag_reg
-            data(fetch_index_reg)(refill_cnt) := io.axi.R.RDATA
-            when(refill_cnt === (WordsPerBlock - 1).U) {
-              valid(fetch_index_reg) := true.B
+            when(!io.axi.R.RLAST) {
+              data(fetch_index_reg)(refill_cnt) := io.axi.R.RDATA
+              tag(fetch_index_reg) := fetch_tag_reg
+            }
+            when(refill_cnt < (WordsPerBlock - 1).U) {
+              refill_cnt := refill_cnt + 1.U
             }
           }
         }
-        when(refill_cnt === (WordsPerBlock - 1).U || !fetch_addr_reg(31).asBool) {
+        when(io.axi.R.RLAST || !fetch_addr_reg(31).asBool) {
+          when(fetch_addr_reg(31).asBool) { valid(fetch_index_reg) := true.B }
           state := state_resp
-        }.otherwise {
-          refill_cnt := refill_cnt + 1.U
-          state := state_refill_req
         }
       }
     }
