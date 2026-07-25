30381e901ddc740f020e0fb27c416e61bea0dee5
tracer-ysyx <tracer@ysyx.org>
2026-07-25 09:38:20 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  09:38:20 up 34 min,  1 user,  load average: 3.73, 3.57, 2.86

--- npc/src/ysyx_26030103_ICache.scala
+++ npc/src/ysyx_26030103_ICache.scala
@@ -71,7 +71,7 @@ class ysyx_26030103_ICache(
   io.axi.AR.ARVALID := false.B
   io.axi.AR.ARID := 0.U
   io.axi.AR.ARADDR := 0.U
-  io.axi.AR.ARLEN := 3.U  // TEST: force burst
+  io.axi.AR.ARLEN := 0.U
   io.axi.AR.ARSIZE := 2.U
   io.axi.AR.ARBURST := 1.U
   io.axi.AR.ARPROT := 0.U
--- ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_pmem.v
+++ ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi_pmem.v
@@ -347,6 +347,8 @@ assign resp_accept_w    = (axi_rvalid_o & axi_rready_i) |
                           (resp_valid_w & resp_is_write_w & !resp_is_last_w) |
                           (resp_valid_w & resp_is_write_w & resp_is_last_w & wr_b_timeout);
 
+always @(posedge clk_i) if(axi_arvalid_i & axi_arready_o) $display("[P] AR len=%d", axi_arlen_i);
+always @(posedge clk_i) if(axi_rvalid_o & axi_rready_i) $display("[P] R last=%d", axi_rlast_o);
 endmodule
 
 //-----------------------------------------------------------------
