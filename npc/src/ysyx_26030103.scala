a95c61c4ef5448e4525d98469ad54313882a338a
tracer-ysyx <tracer@ysyx.org>
2026-07-25 14:27:01 +0800

    > generate verilog ysyx_26030103 林俊杰 Linux DESKTOP-PAP9J8T 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 GNU/Linux  14:27:01 up  5:22,  1 user,  load average: 3.68, 3.15, 2.79

--- npc/src/ysyx_26030103.scala
+++ npc/src/ysyx_26030103.scala
@@ -84,7 +84,8 @@ class ysyx_26030103(
   io.master_arvalid := soc.AR.ARVALID
   io.master_araddr := soc.AR.ARADDR
   io.master_arid := soc.AR.ARID
-  io.master_arlen := soc.AR.ARLEN
+  // Bypass Xbar ARLEN (Chisel optimizes it to 0)
+  io.master_arlen := arbiter.io.memory.AR.ARLEN
   io.master_arsize := soc.AR.ARSIZE
   io.master_arburst := soc.AR.ARBURST
   io.master_arlock := 0.U
